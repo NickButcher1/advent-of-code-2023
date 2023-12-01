f35461a8648e3b7aacb57bbd731452d83d077b18
Nick Butcher <NickButcher1@users.noreply.github.com>
2023-12-01 11:19:27 +0000

    Templates for days 2-10.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
 mod solver01;
+mod solver02;
+mod solver03;
+mod solver04;
+mod solver05;
+mod solver06;
+mod solver07;
+mod solver08;
+mod solver09;
+mod solver10;
 
 use crate::solver01::solve01;
+use crate::solver02::solve02;
+use crate::solver03::solve03;
+use crate::solver04::solve04;
+use crate::solver05::solve05;
+use crate::solver06::solve06;
+use crate::solver07::solve07;
+use crate::solver08::solve08;
+use crate::solver09::solve09;
+use crate::solver10::solve10;
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::time::Instant;
+
+const SOLVER_FUNCTIONS: [fn(Vec<String>) -> (i128, i128); 10] = [
+    // Display row 1 (top edge).
+    solve01, solve02, solve03, solve04, solve05, solve06, solve07, solve08, solve09, solve10,
+];
 
 fn read_input_file(filename: &str) -> Vec<String> {
     let file = match File::open(filename) {
@@ -39,7 +62,7 @@ fn run_one_day(day: i32, is_sample_mode: bool) {
     let (result1, result2) = if input_file.is_empty() {
         (-1, -1)
     } else {
-        solve01(input_file)
+        SOLVER_FUNCTIONS[day as usize - 1](input_file)
     };
 
     let elapsed_ms = time.elapsed().as_nanos() as f64 / 1_000_000.0;
