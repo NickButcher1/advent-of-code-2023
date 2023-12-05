df115d64a8bce88f719cc88777c08b086daf1139
Nick Butcher <NickButcher1@users.noreply.github.com>
2023-12-05 20:19:21 +0000

    Day 5 part 1 in Rust.

--- src/solver05.rs
+++ src/solver05.rs
@@ -1,11 +1,54 @@
-fn solve_part_1(input: Vec<String>) -> i128 {
-    input.len() as i128 // TODO
-}
-
-fn solve_part_2(input: Vec<String>) -> i128 {
-    input.len() as i128 // TODO
-}
+use std::cmp;
 
 pub fn solve05(input: Vec<String>) -> (i128, i128) {
-    (solve_part_1(input.clone()), solve_part_2(input))
+    let total_part_2 = 0;
+
+    let mut seeds: Vec<u64> = Vec::new();
+    let mut maps: Vec<Vec<Vec<u64>>> = vec![vec![vec![]]];
+    let mut current_map_id: i32 = -1;
+
+    for line in input {
+        if line.starts_with("seeds:") {
+            let split_1: Vec<&str> = line.split("seeds: ").collect();
+            seeds = split_1[1]
+                .split(' ')
+                .map(|x| x.parse::<u64>().unwrap())
+                .collect();
+        } else if line.contains(" map:") {
+            current_map_id += 1;
+            maps.push(Vec::new());
+            maps[current_map_id as usize] = Vec::new();
+        } else if !line.is_empty() {
+            let block: Vec<u64> = line.split(' ').map(|x| x.parse::<u64>().unwrap()).collect();
+            maps[current_map_id as usize].push(block);
+        }
+    }
+
+    // Part 1.
+    let mut min_location: u64 = 999999999999999;
+    for seed in seeds {
+        min_location = cmp::min(seed_to_location(seed, &maps), min_location)
+    }
+
+    (min_location as i128, total_part_2 as i128)
+}
+
+fn map_a_to_b(value: u64, map: &Vec<Vec<u64>>) -> u64 {
+    for block in map {
+        let dest = block[0];
+        let src = block[1];
+        let block_len = block[2];
+        if value >= src && value <= (src + block_len - 1) {
+            return value + dest - src;
+        }
+    }
+    value
+}
+
+fn seed_to_location(seed: u64, maps: &Vec<Vec<Vec<u64>>>) -> u64 {
+    let mut current_value = seed;
+    for map in maps {
+        current_value = map_a_to_b(current_value, map)
+    }
+    current_value
 }
