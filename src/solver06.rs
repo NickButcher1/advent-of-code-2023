40142bcf6d6becaa3a47d6e776fe1e2b6e81403b
Nick Butcher <NickButcher1@users.noreply.github.com>
2023-12-06 06:07:42 +0000

    Day 6

--- src/solver06.rs
+++ src/solver06.rs
@@ -1,11 +1,50 @@
-fn solve_part_1(input: Vec<String>) -> i128 {
-    input.len() as i128 // TODO
-}
+fn solve(times: Vec<u64>, distances: Vec<u64>) -> i128 {
+    let mut total = 1;
+    for i in 0..times.len() {
+        let race_time = times[i];
+        let record_distance = distances[i];
+        let mut ways = 0;
 
-fn solve_part_2(input: Vec<String>) -> i128 {
-    input.len() as i128 // TODO
+        for my_time in 0..race_time {
+            let my_speed = my_time;
+            let my_distance = my_speed * (race_time - my_time);
+            if my_distance > record_distance {
+                ways += 1;
+            }
+        }
+        total *= ways;
+    }
+
+    total
 }
 
 pub fn solve06(input: Vec<String>) -> (i128, i128) {
-    (solve_part_1(input.clone()), solve_part_2(input))
+    let times_line: Vec<&str> = input[0].split("Time:").collect();
+    let times_part_1: Vec<u64> = times_line[1]
+        .split(' ')
+        .filter(|s| !s.is_empty())
+        .map(|x| x.parse::<u64>().unwrap())
+        .collect();
+    let times_part_2: Vec<u64> = times_line[1]
+        .replace(' ', "")
+        .split(' ')
+        .map(|x| x.parse::<u64>().unwrap())
+        .collect();
+
+    let distances_line: Vec<&str> = input[1].split("Distance:").collect();
+    let distances_part_1: Vec<u64> = distances_line[1]
+        .split(' ')
+        .filter(|s| !s.is_empty())
+        .map(|x| x.parse::<u64>().unwrap())
+        .collect();
+    let distances_part_2: Vec<u64> = distances_line[1]
+        .replace(' ', "")
+        .split(' ')
+        .map(|x| x.parse::<u64>().unwrap())
+        .collect();
+
+    (
+        solve(times_part_1, distances_part_1),
+        solve(times_part_2, distances_part_2),
+    )
 }
