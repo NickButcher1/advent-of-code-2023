3f85e3dfe6d4ec644d3239415ab4bad466acd185
Nick Butcher <nick.butcher@metaswitch.com>
2023-12-07 18:09:40 +0000

    Day 7 cleanup.

--- src/solver07.rs
+++ src/solver07.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-fn map_card_to_score(card: char, is_part_1: bool) -> u64 {
+fn tiebreak_score_for_card(card: char, is_part_1: bool) -> u64 {
     match card {
         'A' => 14,
         'K' => 13,
@@ -17,18 +17,8 @@ fn map_card_to_score(card: char, is_part_1: bool) -> u64 {
     }
 }
 
-fn decider_hand(hand: &str, is_part_1: bool) -> u64 {
-    let hand_chars = hand.chars().collect::<Vec<char>>();
-    map_card_to_score(hand_chars[4], is_part_1)
-        + map_card_to_score(hand_chars[3], is_part_1) * 100
-        + map_card_to_score(hand_chars[2], is_part_1) * 10000
-        + map_card_to_score(hand_chars[1], is_part_1) * 1000000
-        + map_card_to_score(hand_chars[0], is_part_1) * 100000000
-}
-
-fn score_sorted_c(sorted_c: &Vec<u64>) -> u64 {
-    println!("INPUT: {:?}", sorted_c);
-    if *sorted_c == [5] {
+fn score_for_hand(sorted_c: &Vec<u64>) -> u64 {
+    let score = if *sorted_c == [5] {
         7
     } else if *sorted_c == [1, 4] {
         6
@@ -44,21 +34,32 @@ fn score_sorted_c(sorted_c: &Vec<u64>) -> u64 {
         1
     } else {
         panic!("ERROR");
-    }
+    };
+    score * 10000000000
+}
+
+fn score_for_tiebreak(hand: &str, is_part_1: bool) -> u64 {
+    let hand_chars = hand.chars().collect::<Vec<char>>();
+    tiebreak_score_for_card(hand_chars[4], is_part_1)
+        + tiebreak_score_for_card(hand_chars[3], is_part_1) * 100
+        + tiebreak_score_for_card(hand_chars[2], is_part_1) * 10000
+        + tiebreak_score_for_card(hand_chars[1], is_part_1) * 1000000
+        + tiebreak_score_for_card(hand_chars[0], is_part_1) * 100000000
+}
+
+fn collect_and_sort(counter: &HashMap<char, u64>) -> Vec<u64> {
+    let mut sorted_c: Vec<u64> = counter.values().cloned().collect();
+    sorted_c.sort();
+    sorted_c
 }
 
 fn score_hand_1(hand: &str, counter: HashMap<char, u64>) -> u64 {
-    let mut sorted_c: Vec<u64> = counter.values().cloned().collect();
-    sorted_c.sort();
-    10000000000 * score_sorted_c(&sorted_c) + decider_hand(hand, true)
+    score_for_hand(&collect_and_sort(&counter)) + score_for_tiebreak(hand, true)
 }
 
 fn score_hand_2(hand: &str, counter: &mut HashMap<char, u64>) -> u64 {
-    // Special case for JJJJJ.
     let sorted_c = if hand == "JJJJJ" {
-        let mut output: Vec<u64> = counter.values().cloned().collect();
-        output.sort();
-        output
+        collect_and_sort(counter)
     } else {
         let num_jacks = if counter.contains_key(&'J') {
             counter[&'J']
@@ -66,56 +67,46 @@ fn score_hand_2(hand: &str, counter: &mut HashMap<char, u64>) -> u64 {
             0
         };
         counter.remove(&'J');
-        let mut output: Vec<u64> = counter.values().cloned().collect();
-        output.sort();
+        let mut output: Vec<u64> = collect_and_sort(counter);
         *output.last_mut().unwrap() += num_jacks;
         output
     };
 
-    10000000000 * score_sorted_c(&sorted_c) + decider_hand(hand, false)
+    score_for_hand(&sorted_c) + score_for_tiebreak(hand, false)
 }
 
+fn count_winnings(scored_hands: &Vec<(u64, u64, u64)>) -> u64 {
+    let mut winnings = 0;
+    let mut rank = 0;
+    for (bid, _, _) in scored_hands {
+        rank += 1;
+        winnings += bid * rank;
+    }
+    winnings
+}
 pub fn solve07(input: Vec<String>) -> (i128, i128) {
-    let mut hands: Vec<(&str, HashMap<char, u64>, u64)> = Vec::new();
+    let mut scored_hands: Vec<(u64, u64, u64)> = Vec::new();
 
     for line in &input {
         let split_1: Vec<&str> = line.split(' ').collect();
 
-        // Count the number of each card.
         let mut counter: HashMap<char, u64> = HashMap::new();
         for c in split_1[0].chars() {
             *counter.entry(c).or_insert(0) += 1;
         }
 
-        hands.push((split_1[0], counter, split_1[1].parse().unwrap()));
-    }
-
-    let mut scored_hands: Vec<(u64, u64, u64)> = Vec::new();
-
-    for (hand, mut counter, bid) in hands {
-        println!("{:?}", hand);
         scored_hands.push((
-            bid,
-            score_hand_1(hand, counter.clone()),
-            score_hand_2(hand, &mut counter),
+            split_1[1].parse().unwrap(),
+            score_hand_1(split_1[0], counter.clone()),
+            score_hand_2(split_1[0], &mut counter),
         ))
     }
 
     scored_hands.sort_by_key(|k| k.1);
-    let mut winnings_1 = 0;
-    let mut rank = 0;
-    for (bid, _, _) in &scored_hands {
-        rank += 1;
-        winnings_1 += bid * rank;
-    }
+    let winnings_1 = count_winnings(&scored_hands);
 
     scored_hands.sort_by_key(|k| k.2);
-    let mut winnings_2 = 0;
-    let mut rank = 0;
-    for (bid, _, _) in &scored_hands {
-        rank += 1;
-        winnings_2 += bid * rank;
-    }
+    let winnings_2 = count_winnings(&scored_hands);
 
     (winnings_1 as i128, winnings_2 as i128)
 }
