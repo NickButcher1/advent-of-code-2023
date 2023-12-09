1a56f7e7bdb7a312a1f35f355b0d3c3c3c190f32
Nick Butcher <NickButcher1@users.noreply.github.com>
2023-12-09 07:14:26 +0000

    Day 9: No need to store the last row of zeroes.

--- src/solver09.rs
+++ src/solver09.rs
@@ -6,15 +6,15 @@ pub fn solve09(input: Vec<String>) -> (i128, i128) {
 
     for line in input {
         let mut ints = string_to_vec_i64(&line);
-        let mut heads_of_lists: Vec<i64> = vec![ints[0]];
-        let mut tails_of_lists: Vec<i64> = vec![*ints.last().unwrap()];
+        let mut heads_of_lists: Vec<i64> = Vec::new();
+        let mut tails_of_lists: Vec<i64> = Vec::new();
 
         while !ints.iter().all(|&x| x == 0) {
-            ints = (0..ints.len() - 1).map(|i| ints[i + 1] - ints[i]).collect();
             heads_of_lists.push(ints[0]);
             tails_of_lists.push(*ints.last().unwrap());
+            ints = (0..ints.len() - 1).map(|i| ints[i + 1] - ints[i]).collect();
         }
-
+        
         for i in (0..(heads_of_lists.len() - 1)).rev() {
             heads_of_lists[i] -= heads_of_lists[i + 1];
             tails_of_lists[i] += tails_of_lists[i + 1];
