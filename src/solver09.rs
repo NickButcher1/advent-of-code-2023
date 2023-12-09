075eceae1afafc8d78595cb8d28ca30455c5b1e0
Nick Butcher <NickButcher1@users.noreply.github.com>
2023-12-09 07:04:11 +0000

    Day 9: only need to store first and last from each list.

--- src/solver09.rs
+++ src/solver09.rs
@@ -6,27 +6,22 @@ pub fn solve09(input: Vec<String>) -> (i128, i128) {
 
     for line in input {
         let mut ints = string_to_vec_i64(&line);
-        let mut list_of_lists = vec![ints.clone()];
+        let mut heads_of_lists: Vec<i64> = vec![ints[0]];
+        let mut tails_of_lists: Vec<i64> = vec![*ints.last().unwrap()];
 
         while !ints.iter().all(|&x| x == 0) {
-            let mut new_ints: Vec<i64> = Vec::new();
-            for i in 0..(ints.len() - 1) {
-                new_ints.push(ints[i + 1] - ints[i]);
-            }
-            ints = new_ints.clone();
-            list_of_lists.push(new_ints.clone());
+            ints = (0..ints.len() - 1).map(|i| ints[i + 1] - ints[i]).collect();
+            heads_of_lists.push(ints[0]);
+            tails_of_lists.push(*ints.last().unwrap());
         }
 
-        for i in (0..(list_of_lists.len() - 1)).rev() {
-            let push_value =
-                list_of_lists[i].last().unwrap() + list_of_lists[i + 1].last().unwrap();
-            let insert_value = list_of_lists[i][0] - list_of_lists[i + 1][0];
-            list_of_lists[i].push(push_value);
-            list_of_lists[i].insert(0, insert_value);
+        for i in (0..(heads_of_lists.len() - 1)).rev() {
+            heads_of_lists[i] -= heads_of_lists[i + 1];
+            tails_of_lists[i] += tails_of_lists[i + 1];
         }
 
-        total_part_1 += list_of_lists[0].last().unwrap();
-        total_part_2 += list_of_lists[0][0];
+        total_part_1 += tails_of_lists[0];
+        total_part_2 += heads_of_lists[0];
     }
 
     (total_part_1 as i128, total_part_2 as i128)
