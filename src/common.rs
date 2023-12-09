0f7c50b1dab031e6239b690aa456f3bcbb8f650e
Nick Butcher <NickButcher1@users.noreply.github.com>
2023-12-09 06:24:38 +0000

    Day 9

--- src/common.rs
+++ src/common.rs
@@ -10,3 +10,11 @@ pub fn string_to_vec_u64(input: &str) -> Vec<u64> {
         .map(|x| x.parse::<u64>().unwrap())
         .collect()
 }
+
+pub fn string_to_vec_i64(input: &str) -> Vec<i64> {
+    input
+        .split(' ')
+        .filter(|s| !s.is_empty())
+        .map(|x| x.parse::<i64>().unwrap())
+        .collect()
+}
--- src/solver09.rs
+++ src/solver09.rs
@@ -1,11 +1,33 @@
-fn solve_part_1(input: Vec<String>) -> i128 {
-    input.len() as i128 // TODO
-}
-
-fn solve_part_2(input: Vec<String>) -> i128 {
-    input.len() as i128 // TODO
-}
+use crate::common::string_to_vec_i64;
 
 pub fn solve09(input: Vec<String>) -> (i128, i128) {
-    (solve_part_1(input.clone()), solve_part_2(input))
+    let mut total_part_1: i64 = 0;
+    let mut total_part_2: i64 = 0;
+
+    for line in input {
+        let mut ints = string_to_vec_i64(&line);
+        let mut list_of_lists = vec![ints.clone()];
+
+        while !ints.iter().all(|&x| x == 0) {
+            let mut new_ints: Vec<i64> = Vec::new();
+            for i in 0..(ints.len() - 1) {
+                new_ints.push(ints[i + 1] - ints[i]);
+            }
+            ints = new_ints.clone();
+            list_of_lists.push(new_ints.clone());
+        }
+
+        for i in (0..(list_of_lists.len() - 1)).rev() {
+            let push_value =
+                list_of_lists[i].last().unwrap() + list_of_lists[i + 1].last().unwrap();
+            let insert_value = list_of_lists[i][0] - list_of_lists[i + 1][0];
+            list_of_lists[i].push(push_value);
+            list_of_lists[i].insert(0, insert_value);
+        }
+
+        total_part_1 += list_of_lists[0].last().unwrap();
+        total_part_2 += list_of_lists[0][0];
+    }
+
+    (total_part_1 as i128, total_part_2 as i128)
 }
