df21ff186b836be3668eb9000526d92adf8bdbfa
Nick Butcher <NickButcher1@users.noreply.github.com>
2023-12-09 07:45:19 +0000

    Day 9: really stop now.

--- src/solver09.rs
+++ src/solver09.rs
@@ -1,29 +1,29 @@
 use crate::common::string_to_vec_i64;
 
-// Build up a list of integers. Start from the input, then sum adjecent pairs to make a new list,
+// Build up a list of integers. Start from the input, then sum adjacent pairs to make a new list,
 // one shorter in length than the previous list. Repeat until the new list is all zeroes.
 //
 // We then need to add a new value to the end of each list, but there is an optimisation here -
 // simply sum the last value in each list, which gives the new last value of the first list. That's
-// the score we need for this input.
+// the score we need for this input line.
 //
 // Part 2 can be solved the same way as part 1 - the caller must simply reverse the input.
-pub fn score_line(mut ints: Vec<i64>) -> i64 {
-    let mut tails_of_lists: Vec<i64> = Vec::new();
+pub fn score_line(mut ints: Vec<i64>) -> i128 {
+    let mut score = 0;
 
     while !ints.iter().all(|&x| x == 0) {
-        tails_of_lists.push(*ints.last().unwrap());
+        score += *ints.last().unwrap() as i128;
         ints = (0..ints.len() - 1).map(|i| ints[i + 1] - ints[i]).collect();
     }
 
-    tails_of_lists.iter().sum::<i64>()
+    score
 }
 
 pub fn solve09(input: Vec<String>) -> (i128, i128) {
     (
         input
             .iter()
-            .map(|line| score_line(string_to_vec_i64(line)) as i128)
+            .map(|line| score_line(string_to_vec_i64(line)))
             .sum(),
         input
             .iter()
@@ -33,7 +33,7 @@ pub fn solve09(input: Vec<String>) -> (i128, i128) {
                         .into_iter()
                         .rev()
                         .collect::<Vec<i64>>(),
-                ) as i128
+                )
             })
             .sum(),
     )
