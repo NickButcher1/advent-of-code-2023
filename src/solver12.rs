c01d417e6cf0fe7ee887bf7cbbec6a20fad4c31c
Nick Butcher <NickButcher1@users.noreply.github.com>
2023-12-12 21:45:36 +0000

    Day 12 cleanup: remove type conversions

--- src/solver12.rs
+++ src/solver12.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 
-const OPERATIONAL: u8 = 0;
-const DAMAGED: u8 = 1;
-const UNKNOWN: u8 = 2;
-const UNWANTED: u8 = 3;
+const OPERATIONAL: usize = 0;
+const DAMAGED: usize = 1;
+const UNKNOWN: usize = 2;
+const UNWANTED: usize = 3;
 
 pub fn solve12(input: Vec<String>) -> (i128, i128) {
     (solve_part(input.clone(), false), solve_part(input, true))
@@ -12,7 +12,7 @@ pub fn solve_part(input: Vec<String>, is_part_two: bool) -> i128 {
     let mut sum_arrangements: i128 = 0;
 
     for line in &input {
-        let mut line_ints: Vec<u8> = line
+        let mut line_ints: Vec<usize> = line
             .chars()
             .map(|c| match c {
                 '.' => OPERATIONAL,
@@ -24,9 +24,9 @@ pub fn solve_part(input: Vec<String>, is_part_two: bool) -> i128 {
             .collect();
 
         let split_1: Vec<&str> = line.split(' ').collect();
-        let mut counts: Vec<u8> = split_1[1]
+        let mut counts: Vec<usize> = split_1[1]
             .split(',')
-            .map(|x| x.parse::<u8>().unwrap())
+            .map(|x| x.parse::<usize>().unwrap())
             .collect();
 
         if is_part_two {
@@ -42,7 +42,7 @@ pub fn solve_part(input: Vec<String>, is_part_two: bool) -> i128 {
             }
         }
 
-        let mut cache: HashMap<(Vec<u8>, Vec<u8>), u64> = HashMap::new();
+        let mut cache: HashMap<(Vec<usize>, Vec<usize>), u64> = HashMap::new();
         let arrangements = solve_subset(&line_ints, &counts, &mut cache);
 
         sum_arrangements += arrangements as i128;
@@ -51,9 +51,9 @@ pub fn solve_part(input: Vec<String>, is_part_two: bool) -> i128 {
 }
 
 fn solve_subset(
-    line_ints: &Vec<u8>,
-    counts: &Vec<u8>,
-    cache: &mut HashMap<(Vec<u8>, Vec<u8>), u64>,
+    line_ints: &Vec<usize>,
+    counts: &Vec<usize>,
+    cache: &mut HashMap<(Vec<usize>, Vec<usize>), u64>,
 ) -> u64 {
     return if line_ints.is_empty() {
         0
@@ -66,20 +66,20 @@ fn solve_subset(
         }
 
         // Find all possible positions for the first block.
-        let mut min_for_block: u8 = 0;
+        let mut min_for_block: usize = 0;
         if line_ints[0] != DAMAGED {
-            let mut pos = 0;
+            let mut pos: usize = 0;
             while !can_block_fit_in_pos_forward(line_ints, pos, counts[0]) {
                 pos += 1;
             }
             min_for_block = pos;
         }
 
-        let mut max_for_block: u8 = 0;
+        let mut max_for_block: usize = 0;
         if line_ints[0] == DAMAGED {
             max_for_block = counts[0] - 1;
         } else {
-            let mut pos = line_ints.len() as u8 - 1;
+            let mut pos = line_ints.len() - 1;
             for i in (0..counts.len()).rev() {
                 while !can_block_fit_in_pos_backward(line_ints, pos, counts[i]) {
                     pos -= 1;
@@ -89,14 +89,18 @@ fn solve_subset(
             }
         }
 
+        if max_for_block < min_for_block {
+            return 0;
+        }
+
         // Try all possible positions of the first block.
         let mut sum: u64 = 0;
         // The code from here onwards is terrible and needs a complete rewrite to simplify it!
         for i in 0..(max_for_block - min_for_block - counts[0] + 2) {
-            let offset = min_for_block as usize + i as usize + counts[0] as usize + 1;
+            let offset = min_for_block + i + counts[0] + 1;
 
             // Check if block can actually fit here.
-            let start_pos = min_for_block as usize + i as usize;
+            let start_pos = min_for_block + i;
 
             let mut bad_ops = false;
             if start_pos != 0 {
@@ -115,7 +119,7 @@ fn solve_subset(
                 continue;
             }
 
-            let end_pos = min_for_block as usize + i as usize + counts[0] as usize - 1;
+            let end_pos = min_for_block + i + counts[0] - 1;
             if end_pos != (line_ints.len() - 1) && line_ints[end_pos + 1] == DAMAGED {
                 continue;
             }
@@ -138,7 +142,7 @@ fn solve_subset(
                 sum += x;
             } else {
                 let x = 1;
-                let temp_vec: Vec<u8> = line_ints[(offset - 1)..].to_vec();
+                let temp_vec: Vec<usize> = line_ints[(offset - 1)..].to_vec();
                 let num_damaged_leftover = temp_vec.into_iter().filter(|i| *i == DAMAGED).count();
                 if num_damaged_leftover == 0 {
                     sum += x;
@@ -151,19 +155,19 @@ fn solve_subset(
     };
 }
 
-fn can_block_fit_in_pos_forward(line_ints: &[u8], pos: u8, block_len: u8) -> bool {
+fn can_block_fit_in_pos_forward(line_ints: &[usize], pos: usize, block_len: usize) -> bool {
     for i in 0..block_len {
-        if line_ints[pos as usize + i as usize] == OPERATIONAL {
+        if line_ints[pos + i] == OPERATIONAL {
             return false;
         }
     }
-    let next_id = pos as usize + block_len as usize;
+    let next_id = pos + block_len;
     next_id == line_ints.len() || line_ints[next_id] != DAMAGED
 }
 
-fn can_block_fit_in_pos_backward(line_ints: &[u8], pos: u8, block_len: u8) -> bool {
+fn can_block_fit_in_pos_backward(line_ints: &[usize], pos: usize, block_len: usize) -> bool {
     for i in 0..block_len {
-        if line_ints[pos as usize - i as usize] == OPERATIONAL {
+        if line_ints[pos - i] == OPERATIONAL {
             return false;
         }
     }
