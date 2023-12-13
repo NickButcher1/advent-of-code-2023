91ce63939c875c039ee864f1acf5a9b76f61194e
Nick Butcher <NickButcher1@users.noreply.github.com>
2023-12-13 06:30:46 +0000

    Day 13 part 2.

--- src/solver13.rs
+++ src/solver13.rs
@@ -1,4 +1,4 @@
-const ASH: usize  = 0;
+const ASH: usize = 0;
 const ROCK: usize = 1;
 
 type Board = Vec<Vec<usize>>;
@@ -11,12 +11,14 @@ pub fn solve13(input: Vec<String>) -> (i128, i128) {
 
     for line in &input {
         if line.len() == 0 {
-            println!("RESET");
-            part_1_total += score_board(&board_a, true);
-            part_1_total += score_board(&flip_board(&board_a), false);
+            let (score1, _) = score_board_1(&board_a, true);
+            part_1_total += score1;
+            let (score1, _) = score_board_1(&flip_board(&board_a), false);
+            part_1_total += score1;
+            part_2_total += score_board_part_2(&board_a, true);
+            part_2_total += score_board_part_2(&flip_board(&board_a), false);
             board_a.clear();
         } else {
-            println!("Read line: {}", line);
             let chars: Vec<char> = line.chars().collect();
             let mut row_vec: Vec<usize> = vec![];
 
@@ -26,16 +28,18 @@ pub fn solve13(input: Vec<String>) -> (i128, i128) {
             }
 
             board_a.push(row_vec);
-            // println!("DBG: {:?}", board_a);
         }
     }
     // Get the last board.
-    part_1_total += score_board(&board_a, true);
-    part_1_total += score_board(&flip_board(&board_a), false);
+    let (score1, _) = score_board_1(&board_a, true);
+    part_1_total += score1;
+    let (score1, _) = score_board_1(&flip_board(&board_a), false);
+    part_1_total += score1;
+    part_2_total += score_board_part_2(&board_a, true);
+    part_2_total += score_board_part_2(&flip_board(&board_a), false);
 
     (part_1_total as i128, part_2_total as i128)
 }
-
 
 fn flip_board(board: &Board) -> Board {
     let mut flip_board: Board = vec![];
@@ -48,52 +52,95 @@ fn flip_board(board: &Board) -> Board {
         flip_board.push(row_vec);
     }
 
-    return flip_board;
+    flip_board
 }
-fn score_board(board: &Board, score_rows: bool) -> u64 {
-    println!("SCORE BOARD");
+fn score_board_part_2(board: &Board, score_rows: bool) -> u64 {
+    let (part_1_score, part_1_row) = score_board_1(board, score_rows);
+    // Try all possible flips until we find a new line of reflection.
     let num_rows = board.len();
     let num_cols = board[0].len();
-    println!("({},{})  {:?}", num_rows, num_cols, board);
+    for r in 0..num_rows {
+        for c in 0..num_cols {
+            let mut new_board = board.clone().to_owned();
+            if new_board[r][c] == ASH {
+                new_board[r][c] = ROCK;
+            } else {
+                new_board[r][c] = ASH;
+            }
+
+            let new_score = score_board_2(&new_board, score_rows, part_1_row);
+            if new_score != 0 && new_score != part_1_score {
+                return new_score;
+            }
+        }
+    }
+
+    0
+}
+
+fn score_board_2(board: &Board, score_rows: bool, part_1_row: usize) -> u64 {
+    let num_rows = board.len();
 
     // Assume the line of symmetry is never on the edge of the board.
     let mut score: u64 = 0;
     for r in 1..num_rows {
         // Between r and (r+1) is the line of symmetry.
-        println!("    TRY ROW {}", r);
         let mut is_symmetry = true;
         for d in 0..num_rows {
-
             let top_row: i32 = r as i32 - d as i32 - 1;
             let bottom_row: i32 = r as i32 + d as i32;
-            println!("        TRY DIFF {} COMP ROWS {} v {}", d, top_row, bottom_row);
+            // println!("        TRY DIFF {} COMP ROWS {} v {}", d, top_row, bottom_row);
             // Check if reached edge of board.
             if top_row < 0 {
-                println!("        BREAK OFF TOP");
                 break;
             }
             if bottom_row == num_rows as i32 {
-                println!("        BREAK OFF BOTTOM");
                 break;
             }
             // Check if these two rows are symmetric.
             if board[top_row as usize] != board[bottom_row as usize] {
-                println!("        BREAK NO SYMMETRY");
                 is_symmetry = false;
                 break;
             }
         }
 
-        // Wrong: 18341
-
         if is_symmetry {
-            println!("SYMMETRY AT {} IS_FLIP = {}", r, !score_rows);
-            score += if score_rows {
-                100 * r as u64
-            } else {
-                r as u64
-            };
+            if part_1_row != r {
+                score += if score_rows { 100 * r as u64 } else { r as u64 };
+            }
         }
     }
     score
+}
+
+fn score_board_1(board: &Board, score_rows: bool) -> (u64, usize) {
+    let num_rows = board.len();
+    // Assume the line of symmetry is never on the edge of the board.
+    let mut score: u64 = 0;
+    for r in 1..num_rows {
+        // Between r and (r+1) is the line of symmetry.
+        let mut is_symmetry = true;
+        for d in 0..num_rows {
+            let top_row: i32 = r as i32 - d as i32 - 1;
+            let bottom_row: i32 = r as i32 + d as i32;
+            // Check if reached edge of board.
+            if top_row < 0 {
+                break;
+            }
+            if bottom_row == num_rows as i32 {
+                break;
+            }
+            // Check if these two rows are symmetric.
+            if board[top_row as usize] != board[bottom_row as usize] {
+                is_symmetry = false;
+                break;
+            }
+        }
+
+        if is_symmetry {
+            score += if score_rows { 100 * r as u64 } else { r as u64 };
+            return (score, r);
+        }
+    }
+    (0, 0)
 }
