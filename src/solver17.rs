b16f722faf12e73560abc20914d793d554245ba4
Nick Butcher <NickButcher1@users.noreply.github.com>
2023-12-17 10:02:25 +0000

    Day 17 working but slow

--- src/solver17.rs
+++ src/solver17.rs
@@ -9,41 +9,56 @@ enum Dir {
     Left,
 }
 
+#[derive(Clone, Eq, Hash, PartialEq, Debug)]
+struct Key {
+    r: usize,
+    c: usize,
+    dir: Dir,
+    steps_in_dir: usize,
+}
+
 pub fn solve17(input: &[String]) -> (i128, i128) {
     let board: Board = Board::from_input(input);
     (solve(&board, 1, 3) as i128, solve(&board, 4, 10) as i128)
 }
 
-pub fn solve(board: &Board, min_blocks_in_a_row: usize, max_blocks_in_a_row: usize) -> u64 {
-    let mut cheapest_path_to_reach_end_from: HashMap<(usize, usize, usize, Dir), (usize, usize)> =
-        HashMap::new();
+pub fn solve(board: &Board, min_blocks_in_a_row: usize, max_blocks_in_a_row: usize) -> usize {
+    let mut cheapest_energy_to_reach_end_from: HashMap<Key, usize> = HashMap::new();
 
-    // Cache lookups from (row, column, num in that direction, direction) to (num_moves, energy).
-    cheapest_path_to_reach_end_from.insert((0, 0, 0, Dir::Down), (0, 0));
-    cheapest_path_to_reach_end_from.insert((0, 0, 0, Dir::Right), (0, 0));
+    // Cache lookups from (row, column, direction, num steps in that direction) to energy.
+    cheapest_energy_to_reach_end_from.insert(
+        Key {
+            r: 0,
+            c: 0,
+            dir: Dir::Down,
+            steps_in_dir: 0,
+        },
+        0,
+    );
+    cheapest_energy_to_reach_end_from.insert(
+        Key {
+            r: 0,
+            c: 0,
+            dir: Dir::Right,
+            steps_in_dir: 0,
+        },
+        0,
+    );
 
     let mut best_solution = 999999999;
+    let mut loops_with_no_better_solution = 0;
     loop {
-        for i in 1..=max_blocks_in_a_row {
-            for dir in [Dir::Right, Dir::Down] {
-                let test_key = (board.num_rows - 1, board.num_cols - 1, i, dir);
-                if let Some(cheapest_path) = cheapest_path_to_reach_end_from.get(&test_key) {
-                    if cheapest_path.1 < best_solution {
-                        best_solution = cheapest_path.1;
-                        println!("BEST: {best_solution}");
-                    }
-                }
-            }
-        }
-
         for r in 0..board.num_rows {
             for c in 0..board.num_cols {
                 for dir in [Dir::Up, Dir::Down, Dir::Left, Dir::Right] {
-                    for depth in 0..=max_blocks_in_a_row {
-                        let key = (r, c, depth, dir);
-                        if let Some(cheapest_path) = cheapest_path_to_reach_end_from.get(&key) {
-                            let old_num_moves_in_direction = cheapest_path.0;
-                            let old_energy = cheapest_path.1;
+                    for old_steps_in_dir in 0..=max_blocks_in_a_row {
+                        if let Some(cheapest_energy) = cheapest_energy_to_reach_end_from.get(&Key {
+                            r,
+                            c,
+                            dir,
+                            steps_in_dir: old_steps_in_dir,
+                        }) {
+                            let old_energy = *cheapest_energy;
 
                             for (offset_r, offset_c, new_dir) in [
                                 (0, 1, Dir::Right),
@@ -51,13 +66,13 @@ pub fn solve(board: &Board, min_blocks_in_a_row: usize, max_blocks_in_a_row: usi
                                 (1, 0, Dir::Down),
                                 (-1, 0, Dir::Up),
                             ] {
-                                let new_moves_in_direction = if new_dir == dir {
-                                    old_num_moves_in_direction + 1
+                                let new_steps_in_dir = if new_dir == dir {
+                                    old_steps_in_dir + 1
                                 } else {
                                     // Change of direction is not allowed if:
                                     // - we haven't done the minimum allowed number of moves in that direction
                                     // - it goes back to where we just came from.
-                                    if old_num_moves_in_direction < min_blocks_in_a_row
+                                    if old_steps_in_dir < min_blocks_in_a_row
                                         || new_dir == Dir::Up && dir == Dir::Down
                                         || new_dir == Dir::Down && dir == Dir::Up
                                         || new_dir == Dir::Left && dir == Dir::Right
@@ -68,7 +83,7 @@ pub fn solve(board: &Board, min_blocks_in_a_row: usize, max_blocks_in_a_row: usi
                                     1
                                 };
 
-                                if new_moves_in_direction > max_blocks_in_a_row {
+                                if new_steps_in_dir > max_blocks_in_a_row {
                                     continue;
                                 }
 
@@ -89,24 +104,23 @@ pub fn solve(board: &Board, min_blocks_in_a_row: usize, max_blocks_in_a_row: usi
                                         .to_digit(10)
                                         .unwrap() as usize;
 
-                                let new_key = (
-                                    new_r as usize,
-                                    new_c as usize,
-                                    new_moves_in_direction,
-                                    new_dir,
-                                );
+                                let new_key = Key {
+                                    r: new_r as usize,
+                                    c: new_c as usize,
+                                    steps_in_dir: new_steps_in_dir,
+                                    dir: new_dir,
+                                };
 
-                                if let Some(existing_cheapest_path) =
-                                    cheapest_path_to_reach_end_from.get(&new_key)
+                                if let Some(existing_cheapest_energy) =
+                                    cheapest_energy_to_reach_end_from.get(&new_key)
                                 {
-                                    let this_old_energy = existing_cheapest_path.1;
+                                    let this_old_energy = *existing_cheapest_energy;
                                     if new_energy < this_old_energy {
-                                        cheapest_path_to_reach_end_from
-                                            .insert(new_key, (new_moves_in_direction, new_energy));
+                                        cheapest_energy_to_reach_end_from
+                                            .insert(new_key, new_energy);
                                     }
                                 } else {
-                                    cheapest_path_to_reach_end_from
-                                        .insert(new_key, (new_moves_in_direction, new_energy));
+                                    cheapest_energy_to_reach_end_from.insert(new_key, new_energy);
                                 }
                             }
                         }
@@ -114,7 +128,27 @@ pub fn solve(board: &Board, min_blocks_in_a_row: usize, max_blocks_in_a_row: usi
                 }
             }
         }
-    }
 
-    0 // TODO
+        loops_with_no_better_solution += 1;
+        for steps_in_dir in 1..=max_blocks_in_a_row {
+            for dir in [Dir::Right, Dir::Down] {
+                let test_key = Key {
+                    r: board.num_rows - 1,
+                    c: board.num_cols - 1,
+                    dir,
+                    steps_in_dir,
+                };
+
+                if let Some(cheapest_energy) = cheapest_energy_to_reach_end_from.get(&test_key) {
+                    if *cheapest_energy < best_solution {
+                        best_solution = *cheapest_energy;
+                        loops_with_no_better_solution = 0;
+                    }
+                }
+            }
+        }
+        if loops_with_no_better_solution == 10 {
+            return best_solution;
+        }
+    }
 }
