7db80c6ecc8f320c1dd15b07f0517a289ed150b1
Nick Butcher <NickButcher1@users.noreply.github.com>
2023-12-22 20:12:09 +0000

    2015 day 2

--- year2015/src/lib.rs
+++ year2015/src/lib.rs
@@ -1,6 +1,8 @@
 pub mod solver01;
+pub mod solver02;
 
 use crate::solver01::solve01;
+use crate::solver02::solve02;
 
 type SolverFunction = fn(&[String]) -> (i128, i128);
 
@@ -10,7 +12,7 @@ pub fn solve_blank(_input: &[String]) -> (i128, i128) {
 
 pub const SOLVER_FUNCTIONS: [SolverFunction; 25] = [
     solve01,
-    solve_blank,
+    solve02,
     solve_blank,
     solve_blank,
     solve_blank,
---
+++ year2015/src/solver02.rs
@@ -0,0 +1,17 @@
+use aoc::input::string_to_vec_u64;
+
+pub fn solve02(input: &[String]) -> (i128, i128) {
+    let answer = input
+        .iter()
+        .fold((0, 0), |(part_1_area, part_2_len), line| {
+            let mut xyz = string_to_vec_u64(line, 'x');
+            xyz.sort();
+            (
+                // One extra for the smallest of the six sides.
+                part_1_area + 3 * xyz[0] * xyz[1] + 2 * xyz[0] * xyz[2] + 2 * xyz[1] * xyz[2],
+                // Volume + perimeter of smallest side.
+                part_2_len + xyz[0] * xyz[1] * xyz[2] + 2 * xyz[0] + 2 * xyz[1],
+            )
+        });
+    (answer.0 as i128, answer.1 as i128)
+}
