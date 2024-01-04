9b3b7fcc5fdc1e08630f2f68b2a6b80b47a96351
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-01-04 13:00:33 +0000

    2015 day 4

--- year2015/src/lib.rs
+++ year2015/src/lib.rs
@@ -1,10 +1,12 @@
 pub mod solver01;
 pub mod solver02;
 pub mod solver03;
+pub mod solver04;
 
 use crate::solver01::solve01;
 use crate::solver02::solve02;
 use crate::solver03::solve03;
+use crate::solver04::solve04;
 
 type SolverFunction = fn(&[String]) -> (i128, i128);
 
@@ -16,7 +18,7 @@ pub const SOLVER_FUNCTIONS: [SolverFunction; 25] = [
     solve01,
     solve02,
     solve03,
-    solve_blank,
+    solve04,
     solve_blank,
     solve_blank,
     solve_blank,
---
+++ year2015/src/solver04.rs
@@ -0,0 +1,21 @@
+use md5;
+
+pub fn solve04(input: &[String]) -> (i128, i128) {
+    let key = &input[0];
+    let mut test_val = 0;
+    let mut part_1 = 0;
+
+    loop {
+        test_val += 1;
+        let hash = md5::compute(format!("{key}{test_val}"));
+        if hash[0] == 0 && hash[1] == 0 {
+            if part_1 == 0 && hash[2] < 16 {
+                part_1 = test_val
+            }
+            if hash[2] == 0 {
+                break;
+            }
+        }
+    }
+    (part_1, test_val)
+}
