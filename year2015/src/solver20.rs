41b17d230c1b0c56a1825da2657f608b1cdef6e1
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-01-05 10:01:53 +0000

    2015 day 20 part 2

--- year2015/src/solver20.rs
+++ year2015/src/solver20.rs
@@ -1,20 +1,30 @@
 pub fn solve20(input: &[String]) -> (i128, i128) {
     let target = input[0].parse::<usize>().unwrap();
+    (
+        solve(target, usize::MAX, 10) as i128,
+        solve(target, 50, 11) as i128,
+    )
+}
+
+pub fn solve(target: usize, elf_steps: usize, presents_multiplier: usize) -> usize {
     let mut presents_per_house: Vec<usize> = vec![0; target / 10];
 
     for elf in 1..target / 10 {
-        for house in (elf..target / 10).step_by(elf) {
-            presents_per_house[house] += elf * 10;
+        for house in (elf..target / 10).step_by(elf).take(elf_steps) {
+            presents_per_house[house] += elf * presents_multiplier;
         }
     }
 
-    let mut part_1 = 0;
-    for house in 1..target / 10 {
-        if presents_per_house[house] >= target {
-            part_1 = house;
-            break;
+    for (house, presents) in presents_per_house
+        .iter()
+        .enumerate()
+        .take(target / 10)
+        .skip(1)
+    {
+        if *presents >= target {
+            return house;
         }
     }
 
-    (part_1 as i128, 0)
+    unreachable!();
 }
