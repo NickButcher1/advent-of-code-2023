0a91bcde0e6cd1d091802e2b76ab9019c91d7fd9
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-01-07 20:31:20 +0000

    2017 day 2

---
+++ year2017/src/solver02.rs
@@ -0,0 +1,20 @@
+use aoc::input::string_to_vec_u64;
+
+pub fn solve02(input: &[String]) -> (i128, i128) {
+    let (part_1, part_2) = input.iter().fold((0, 0), |(part_1, mut part_2), line| {
+        let numbers = string_to_vec_u64(line, '\t');
+        let max_min_diff = numbers.iter().max().unwrap() - numbers.iter().min().unwrap();
+
+        for i in 0..numbers.len() {
+            for j in 0..numbers.len() {
+                if numbers[i] > numbers[j] && numbers[i] % numbers[j] == 0 {
+                    part_2 += numbers[i] / numbers[j];
+                }
+            }
+        }
+
+        (part_1 + max_min_diff, part_2)
+    });
+
+    (part_1 as i128, part_2 as i128)
+}
