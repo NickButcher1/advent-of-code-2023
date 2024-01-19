7dd2bc9a4e25a13a388b208ddcf2857975d70c07
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-01-19 12:02:07 +0000

    2017 day 4

--- year2017/src/lib.rs
+++ year2017/src/lib.rs
@@ -1,9 +1,11 @@
 pub mod solver01;
 pub mod solver02;
+pub mod solver04;
 pub mod solver20;
 
 use crate::solver01::solve01;
 use crate::solver02::solve02;
+use crate::solver04::solve04;
 use crate::solver20::solve20;
 
 type SolverFunction = fn(&[String]) -> (i128, i128);
@@ -16,7 +18,7 @@ pub const SOLVER_FUNCTIONS: [SolverFunction; 25] = [
     solve01,
     solve02,
     solve_blank,
-    solve_blank,
+    solve04,
     solve_blank,
     solve_blank,
     solve_blank,
---
+++ year2017/src/solver04.rs
@@ -0,0 +1,34 @@
+use std::collections::HashSet;
+
+pub fn solve04(input: &[String]) -> (i128, i128) {
+    let part_1 = input.iter().fold(0, |acc, line| {
+        let strings_list: Vec<&str> = line.split(' ').collect();
+        let strings_set: HashSet<&str> = strings_list.clone().into_iter().collect();
+        if strings_list.len() == strings_set.len() {
+            acc + 1
+        } else {
+            acc
+        }
+    });
+
+    let part_2 = input.iter().fold(0, |acc, line| {
+        let strings_list_unsorted: Vec<&str> = line.split(' ').collect();
+        let strings_list: Vec<String> = strings_list_unsorted
+            .clone()
+            .into_iter()
+            .map(|s| {
+                let mut chars: Vec<char> = s.chars().collect();
+                chars.sort_unstable();
+                chars.into_iter().collect()
+            })
+            .collect();
+        let strings_set: HashSet<String> = strings_list.clone().into_iter().collect();
+        if strings_list.len() == strings_set.len() {
+            acc + 1
+        } else {
+            acc
+        }
+    });
+
+    (part_1, part_2)
+}
