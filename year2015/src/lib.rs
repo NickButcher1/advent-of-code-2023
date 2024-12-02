8986a78dc62a3722c1a391f4ce86d5e43b04b335
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-12-02 11:03:31 +0000

    2015 day 23

--- year2015/src/lib.rs
+++ year2015/src/lib.rs
@@ -10,6 +10,7 @@ pub mod solver14;
 pub mod solver15;
 pub mod solver18;
 pub mod solver20;
+pub mod solver23;
 
 use crate::solver01::solve01;
 use crate::solver02::solve02;
@@ -23,6 +24,7 @@ use crate::solver14::solve14;
 use crate::solver15::solve15;
 use crate::solver18::solve18;
 use crate::solver20::solve20;
+use crate::solver23::solve23;
 
 type SolverFunction = fn(&[String]) -> (i128, i128);
 
@@ -53,7 +55,7 @@ pub const SOLVER_FUNCTIONS: [SolverFunction; 25] = [
     solve20,
     solve_blank,
     solve_blank,
-    solve_blank,
+    solve23,
     solve_blank,
     solve_blank,
 ];
---
+++ year2015/src/solver23.rs
@@ -0,0 +1,65 @@
+use regex::Regex;
+
+fn get_offset(offset_str: &str) -> i32 {
+    offset_str.parse().expect("Not a valid number")
+}
+
+fn get_register_index(register_str: &str) -> usize {
+    match register_str {
+        "a" => 0,
+        "b" => 1,
+        _ => panic!("Invalid input"),
+    }
+}
+
+pub fn solve23(input: &[String]) -> (i128, i128) {
+    (solve(input, 0) as i128, solve(input, 1) as i128)
+}
+
+pub fn solve(input: &[String], initial_a: i32) -> i128 {
+    let re_jmp = Regex::new(r"^jmp ([+-]\d+)$").unwrap();
+    let re_jie = Regex::new(r"^jie ([a-b]), ([+-]\d+)$").unwrap();
+    let re_jio = Regex::new(r"^jio ([a-b]), ([+-]\d+)$").unwrap();
+
+    let mut registers = vec![initial_a, 0];
+    let mut instruction_pointer: i32 = 0;
+
+    while instruction_pointer >= 0 && instruction_pointer < input.len() as i32 {
+        let instruction = &input[instruction_pointer as usize] as &str;
+        instruction_pointer += 1;
+        match instruction {
+            "hlf a" => registers[0] /= 2,
+            "hlf b" => registers[1] /= 2,
+            "tpl a" => registers[0] *= 3,
+            "tpl b" => registers[1] *= 3,
+            "inc a" => registers[0] += 1,
+            "inc b" => registers[1] += 1,
+
+            _ if re_jmp.is_match(instruction) => {
+                let caps = re_jmp.captures(instruction).unwrap();
+                let offset = get_offset(&caps[1]);
+                instruction_pointer += offset - 1;
+            }
+            _ if re_jie.is_match(instruction) => {
+                let caps = re_jie.captures(instruction).unwrap();
+                let register_index = get_register_index(&caps[1]);
+                let offset = get_offset(&caps[2]);
+                if registers[register_index] % 2 == 0 {
+                    instruction_pointer += offset - 1;
+                }
+            }
+            _ if re_jio.is_match(instruction) => {
+                let caps = re_jio.captures(instruction).unwrap();
+                let register_index = get_register_index(&caps[1]);
+                let offset = get_offset(&caps[2]);
+                if registers[register_index] == 1 {
+                    instruction_pointer += offset - 1;
+                }
+            }
+            _ => {
+                panic!("NO MATCH on {instruction}");
+            }
+        }
+    }
+    registers[1] as i128
+}
