346fd39b0b99c74a2852ab98f82c6827e2fdabb3
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-12-04 06:05:11 +0000

    2024 day 4 Cleanup the code and make it functional.

--- year2024/src/solver04.rs
+++ year2024/src/solver04.rs
@@ -1,113 +1,72 @@
 use aoc::board::Board;
 
+fn count_xmas(board: &Board, x: usize, y: usize, dx: isize, dy: isize) -> usize {
+    let xmas = ["XMAS", "SAMX"];
+
+    xmas.iter()
+        .filter(|&&xmas| {
+            let chars: Vec<char> = xmas.chars().collect();
+            let len = chars.len();
+
+            for i in 0..len {
+                let nx = x as isize + i as isize * dx;
+                let ny = y as isize + i as isize * dy;
+
+                if nx < 0
+                    || ny < 0
+                    || nx >= board.num_cols as isize
+                    || ny >= board.num_rows as isize
+                {
+                    return false;
+                }
+
+                if board.cells[nx as usize][ny as usize] != chars[i] {
+                    return false;
+                }
+            }
+
+            return true;
+        })
+        .count()
+}
+
+fn is_mas_cross(board: &Board, x: usize, y: usize) -> bool {
+    let valid_m_and_s = [
+        (('M', 'S'), ('M', 'S')),
+        (('M', 'S'), ('S', 'M')),
+        (('S', 'M'), ('S', 'M')),
+        (('S', 'M'), ('M', 'S')),
+    ];
+
+    valid_m_and_s
+        .iter()
+        .any(|&(left_diagonal, right_diagonal)| {
+            board.cells[x - 1][y - 1] == left_diagonal.0
+                && board.cells[x + 1][y + 1] == left_diagonal.1
+                && board.cells[x - 1][y + 1] == right_diagonal.0
+                && board.cells[x + 1][y - 1] == right_diagonal.1
+        })
+}
+
 pub fn solve04(input: &[String]) -> (i128, i128) {
     let board = Board::from_input(input);
 
     let mut xmas_count_1 = 0;
 
-    for x in 0..(board.num_cols) {
-        for y in 0..(board.num_rows) {
-            if (y < (board.num_cols - 3))
-                && board.cells[x][y] == 'X'
-                && board.cells[x][y + 1] == 'M'
-                && board.cells[x][y + 2] == 'A'
-                && board.cells[x][y + 3] == 'S'
-            {
-                xmas_count_1 += 1;
-            }
-
-            if (x < (board.num_rows - 3))
-                && board.cells[x][y] == 'X'
-                && board.cells[x + 1][y] == 'M'
-                && board.cells[x + 2][y] == 'A'
-                && board.cells[x + 3][y] == 'S'
-            {
-                xmas_count_1 += 1;
-            }
-
-            if (x < (board.num_cols - 3))
-                && (y < (board.num_rows - 3))
-                && board.cells[x][y] == 'X'
-                && board.cells[x + 1][y + 1] == 'M'
-                && board.cells[x + 2][y + 2] == 'A'
-                && board.cells[x + 3][y + 3] == 'S'
-            {
-                xmas_count_1 += 1;
-            }
-
-            if (x < (board.num_cols - 3))
-                && y >= 3
-                && board.cells[x][y] == 'X'
-                && board.cells[x + 1][y - 1] == 'M'
-                && board.cells[x + 2][y - 2] == 'A'
-                && board.cells[x + 3][y - 3] == 'S'
-            {
-                xmas_count_1 += 1;
-            }
-
-            if (y < (board.num_cols - 3))
-                && board.cells[x][y] == 'S'
-                && board.cells[x][y + 1] == 'A'
-                && board.cells[x][y + 2] == 'M'
-                && board.cells[x][y + 3] == 'X'
-            {
-                xmas_count_1 += 1;
-            }
-
-            if (x < (board.num_rows - 3))
-                && board.cells[x][y] == 'S'
-                && board.cells[x + 1][y] == 'A'
-                && board.cells[x + 2][y] == 'M'
-                && board.cells[x + 3][y] == 'X'
-            {
-                xmas_count_1 += 1;
-            }
-
-            if (x < (board.num_cols - 3))
-                && (y < (board.num_rows - 3))
-                && board.cells[x][y] == 'S'
-                && board.cells[x + 1][y + 1] == 'A'
-                && board.cells[x + 2][y + 2] == 'M'
-                && board.cells[x + 3][y + 3] == 'X'
-            {
-                xmas_count_1 += 1;
-            }
-
-            if (x < (board.num_cols - 3))
-                && y >= 3
-                && board.cells[x][y] == 'S'
-                && board.cells[x + 1][y - 1] == 'A'
-                && board.cells[x + 2][y - 2] == 'M'
-                && board.cells[x + 3][y - 3] == 'X'
-            {
-                xmas_count_1 += 1;
-            }
+    for x in 0..board.num_cols {
+        for y in 0..board.num_rows {
+            xmas_count_1 += count_xmas(&board, x, y, 0, 1) + // Horizontal
+            count_xmas(&board, x, y, 1, 0) + // Vertical
+            count_xmas(&board, x, y, 1, 1) + // Diagonal Down-Right
+            count_xmas(&board, x, y, 1, -1); // Diagonal Up-Right
         }
     }
 
     let mut xmas_count_2 = 0;
     for x in 1..(board.num_cols - 1) {
         for y in 1..(board.num_rows - 1) {
-            if board.cells[x][y] == 'A' {
-                if (board.cells[x - 1][y - 1] == 'M'
-                    && board.cells[x + 1][y + 1] == 'S'
-                    && board.cells[x - 1][y + 1] == 'M'
-                    && board.cells[x + 1][y - 1] == 'S')
-                    || (board.cells[x - 1][y - 1] == 'M'
-                        && board.cells[x + 1][y + 1] == 'S'
-                        && board.cells[x - 1][y + 1] == 'S'
-                        && board.cells[x + 1][y - 1] == 'M')
-                    || (board.cells[x - 1][y - 1] == 'S'
-                        && board.cells[x + 1][y + 1] == 'M'
-                        && board.cells[x - 1][y + 1] == 'S'
-                        && board.cells[x + 1][y - 1] == 'M')
-                    || (board.cells[x - 1][y - 1] == 'S'
-                        && board.cells[x + 1][y + 1] == 'M'
-                        && board.cells[x - 1][y + 1] == 'M'
-                        && board.cells[x + 1][y - 1] == 'S')
-                {
-                    xmas_count_2 += 1;
-                }
+            if board.cells[x][y] == 'A' && is_mas_cross(&board, x, y) {
+                xmas_count_2 += 1;
             }
         }
     }
