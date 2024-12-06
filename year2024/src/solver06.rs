9db2cf84d8e9b15d178ecb65c009ccd83ff4e703
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-12-06 06:00:04 +0000

    2024 Day 6 cleanup

--- year2024/src/solver06.rs
+++ year2024/src/solver06.rs
@@ -7,6 +7,8 @@ const GUARD_START: char = '^';
 const GUARD_PATH: char = 'X';
 const BORDER: char = 'O';
 
+const GUARD_MOVES: [(isize, isize); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+
 fn parse_input(input: &[String]) -> ((usize, usize), usize, Board) {
     let mut board = Board::from_input(input);
     board.add_border(BORDER);
@@ -16,30 +18,36 @@ fn parse_input(input: &[String]) -> ((usize, usize), usize, Board) {
     (guard_pos, 0, board)
 }
 
-fn find_path(mut guard_pos: (usize, usize), mut guard_direction: usize, board: &mut Board) -> usize {
-    let guard_moves: Vec<(isize, isize)> = vec![(-1, 0), (0, 1), (1, 0), (0, -1)]; // degrees is index.
-    let max_steps = (board.num_rows -2) * (board.num_cols - 2);
-    let mut solution = 0;
-    let mut loop_count = 0;
-    loop {
-        loop_count += 1;
-        let next_pos = ((guard_pos.0 as isize + guard_moves[guard_direction].0) as usize, (guard_pos.1 as isize + guard_moves[guard_direction].1) as usize);
+fn find_path(
+    mut guard_pos: (usize, usize),
+    mut guard_direction: usize,
+    board: &mut Board,
+) -> usize {
+    // Could make a lower upper bound by counting empty cells, but it really doesn't matter.
+    let max_steps = (board.num_rows - 2) * (board.num_cols - 2);
+
+    for _ in 1..=max_steps {
+        let (x, y) = guard_pos;
+        let (dx, dy) = GUARD_MOVES[guard_direction];
+        let next_pos = ((x as isize + dx) as usize, (y as isize + dy) as usize);
         let next_cell = board.cells[next_pos.0][next_pos.1];
-        if loop_count > max_steps {
-            break;
-        } else if next_cell == BORDER {
-            solution = board.count(GUARD_PATH);
-            break;
-        } else if next_cell == OBSTRUCTION {
-            guard_direction += 1;
-            guard_direction %= 4;
-        } else if next_cell == EMPTY || next_cell == GUARD_PATH {
-            guard_pos = next_pos;
-            board.cells[next_pos.0][next_pos.1] = GUARD_PATH;
+
+        match next_cell {
+            BORDER => {
+                return board.count(GUARD_PATH) as usize;
+            }
+            OBSTRUCTION => {
+                guard_direction = (guard_direction + 1) % 4;
+            }
+            EMPTY | GUARD_PATH => {
+                guard_pos = next_pos;
+                board.cells[next_pos.0][next_pos.1] = GUARD_PATH;
+            }
+            _ => (),
         }
     }
 
-    solution as usize
+    0
 }
 
 pub fn solve06(input: &[String]) -> (i128, i128) {
@@ -58,8 +66,6 @@ pub fn solve06(input: &[String]) -> (i128, i128) {
             if steps == 0 {
                 solution_two += 1;
             }
-        } else if board.cells[r][c] == OBSTRUCTION || board.cells[r][c] == GUARD_PATH {
-            // Skip
         }
     }
 
