caeefeddd4797405bac5637da16a6dfd5ca5fc51
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-12-09 07:33:32 +0000

    2024 days 8 and 9 pre-cleanup

--- year2024/src/solver08.rs
+++ year2024/src/solver08.rs
@@ -1,3 +1,77 @@
+use aoc::board::Board;
+use itertools::{iproduct, Itertools};
+use std::collections::HashMap;
+
+const EMPTY: char = '.';
+const ANTINODE: char = '#';
+
+fn solve(board: &Board, antennas: &HashMap<char, Vec<(usize, usize)>>, is_part_two: bool) -> u64 {
+    let mut antinode_board = Board::create_empty(board.num_rows, board.num_cols, EMPTY);
+
+    for ch in antennas.keys() {
+        let mut combinations = antennas.get(ch).unwrap().into_iter().combinations(2);
+        // Coordinates are r,c.
+        while let Some(combo) = combinations.next() {
+            let dr = combo[0].0.abs_diff(combo[1].0);
+            let dc = combo[0].1.abs_diff(combo[1].1);
+
+            let mut combo2 = combo.clone();
+            combo2.sort_by_key(|coord| coord.1);
+
+            let (mult_min, mult_max) = if is_part_two { (0, 100) } else { (1, 1) };
+
+            for mult in mult_min..=mult_max {
+                let antinode1_c = combo2[0].1 as i32 - mult * dc as i32;
+                let antinode2_c = combo2[1].1 as i32 + mult * dc as i32;
+
+                let (antinode1_r, antinode2_r) = if combo2[1].0 >= combo2[0].0 {
+                    (
+                        combo2[0].0 as i32 - mult * dr as i32,
+                        combo2[1].0 as i32 + mult * dr as i32,
+                    )
+                } else {
+                    (
+                        combo2[0].0 as i32 + mult * dr as i32,
+                        combo2[1].0 as i32 - mult * dr as i32,
+                    )
+                };
+                if antinode1_r >= 0
+                    && antinode1_c >= 0
+                    && (antinode1_r as usize) < board.num_rows
+                    && (antinode1_c as usize) < board.num_cols
+                {
+                    antinode_board.cells[antinode1_r as usize][antinode1_c as usize] = ANTINODE;
+                }
+                if antinode2_r >= 0
+                    && antinode2_c >= 0
+                    && (antinode2_r as usize) < board.num_rows
+                    && (antinode2_c as usize) < board.num_cols
+                {
+                    antinode_board.cells[antinode2_r as usize][antinode2_c as usize] = ANTINODE;
+                }
+            }
+        }
+    }
+
+    antinode_board.count(ANTINODE)
+}
+
 pub fn solve08(input: &[String]) -> (i128, i128) {
-    (0 as i128, 0 as i128)
+    let board = Board::from_input(input);
+
+    // Make HashMap from char -> list of x,y tuple.
+    let mut antennas: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
+    for (c, r) in iproduct!(0..board.num_cols, 0..board.num_rows) {
+        if board.cells[r][c] != EMPTY {
+            antennas
+                .entry(board.cells[r][c])
+                .and_modify(|v| v.push((r, c)))
+                .or_insert(vec![(r, c)]);
+        }
+    }
+
+    (
+        solve(&board, &antennas, false) as i128,
+        solve(&board, &antennas, true) as i128,
+    )
 }
--- year2024/src/solver09.rs
+++ year2024/src/solver09.rs
@@ -1,3 +1,110 @@
+fn add_file(output: &mut Vec<i32>, num_bytes: i32, id: i32) {
+    for _ in 0..num_bytes {
+        output.push(id);
+    }
+}
+
+fn add_free_space(output: &mut Vec<i32>, num_bytes: i32) {
+    for _ in 0..num_bytes {
+        output.push(i32::MAX);
+    }
+}
+
 pub fn solve09(input: &[String]) -> (i128, i128) {
-    (0 as i128, 0 as i128)
+    // Alternatives file / free space.  Each has ID number from zero in original order.
+    let disk_map = input[0]
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as i32)
+        .collect::<Vec<i32>>();
+
+    let mut files: Vec<(i32, i32, i32)> = vec![];
+
+    let mut disk_layout = vec![];
+    let mut id = 0;
+    // TODO just flip a bool.
+    for chunk in disk_map.chunks(2) {
+        match chunk {
+            [a, b] => {
+                // ID, len, start_index;
+                files.push((id, *a, disk_layout.len() as i32));
+                add_file(&mut disk_layout, *a, id);
+                add_free_space(&mut disk_layout, *b);
+            }
+            [a] => {
+                files.push((id, *a, disk_layout.len() as i32));
+                add_file(&mut disk_layout, *a, id);
+            }
+            _ => unreachable!(),
+        }
+        id += 1;
+    }
+    let original_disk_layout = disk_layout.clone();
+    let mut l_index = 0;
+    let mut r_index = disk_layout.len() - 1;
+    while r_index > l_index {
+        if disk_layout[l_index] != i32::MAX {
+            l_index += 1;
+        } else if disk_layout[r_index] == i32::MAX {
+            r_index -= 1;
+        } else {
+            // Move from right to left.
+            disk_layout[l_index] = disk_layout[r_index];
+            disk_layout[r_index] = i32::MAX;
+            r_index -= 1;
+            l_index += 1;
+        }
+    }
+
+    let mut checksum: i64 = 0;
+    l_index = 0;
+    loop {
+        let this_value = disk_layout[l_index];
+        if this_value == i32::MAX {
+            break;
+        }
+        checksum += l_index as i64 * this_value as i64;
+        l_index += 1;
+    }
+
+    disk_layout = original_disk_layout;
+    for (id, len, start_index) in files.into_iter().rev() {
+        // Find first free slot with at least len bytes.
+        let mut first_free_slot_index = 0;
+        let mut is_gap = false;
+        'outer_loop: while first_free_slot_index < start_index {
+            let mut gap_len = 0;
+            for i in 0..len {
+                if disk_layout[(first_free_slot_index + i) as usize] == i32::MAX {
+                    gap_len += 1;
+                    if gap_len == len {
+                        is_gap = true;
+                        break 'outer_loop;
+                    }
+                } else {
+                    gap_len = 0;
+                }
+            }
+            first_free_slot_index += 1;
+        }
+
+        // Move file.
+        if is_gap {
+            for i in 0..len {
+                disk_layout[(first_free_slot_index + i) as usize] = id;
+                disk_layout[(start_index + i) as usize] = i32::MAX;
+            }
+        }
+    }
+
+    let mut checksum2: i64 = 0;
+    l_index = 0;
+    while l_index < disk_layout.len() {
+        let this_value = disk_layout[l_index];
+        if this_value != i32::MAX {
+            checksum2 += l_index as i64 * this_value as i64;
+        }
+        l_index += 1;
+    }
+
+    (checksum as i128, checksum2 as i128)
 }
