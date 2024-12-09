55962244da8fa4ec1cde6ae3c58bbeee06a37ed2
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-12-09 10:17:29 +0000

    2024 day 9 cleanup

--- year2024/src/solver09.rs
+++ year2024/src/solver09.rs
@@ -1,82 +1,83 @@
-fn add_file(output: &mut Vec<i32>, num_bytes: i32, id: i32) {
+const EMPTY_BLOCK: usize = usize::MAX;
+
+fn add_file(output: &mut Vec<usize>, num_bytes: usize, id: usize) {
     for _ in 0..num_bytes {
         output.push(id);
     }
 }
 
-fn add_free_space(output: &mut Vec<i32>, num_bytes: i32) {
+fn add_free_space(output: &mut Vec<usize>, num_bytes: usize) {
     for _ in 0..num_bytes {
-        output.push(i32::MAX);
+        output.push(EMPTY_BLOCK);
     }
 }
 
-pub fn solve09(input: &[String]) -> (i128, i128) {
-    // Alternatives file / free space.  Each has ID number from zero in original order.
+fn parse_input(input: &[String]) -> (Vec<(usize, usize, usize)>, Vec<usize>) {
     let disk_map = input[0]
         .chars()
-        .map(|c| c.to_digit(10).unwrap() as i32)
-        .collect::<Vec<i32>>();
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect::<Vec<usize>>();
 
-    let mut files: Vec<(i32, i32, i32)> = vec![];
+    // Files in a list of (ID, len, start_index) tuples.
+    disk_map.chunks(2).enumerate().fold(
+        (vec![], vec![]),
+        |(mut files, mut disk_layout), (id, chunk)| {
+            match chunk {
+                [a, b] => {
+                    files.push((id, *a, disk_layout.len()));
+                    add_file(&mut disk_layout, *a, id);
+                    add_free_space(&mut disk_layout, *b);
+                }
+                [a] => {
+                    files.push((id, *a, disk_layout.len()));
+                    add_file(&mut disk_layout, *a, id);
+                }
+                _ => unreachable!(),
+            }
+            (files, disk_layout)
+        },
+    )
+}
 
-    let mut disk_layout = vec![];
-    let mut id = 0;
-    // TODO just flip a bool.
-    for chunk in disk_map.chunks(2) {
-        match chunk {
-            [a, b] => {
-                // ID, len, start_index;
-                files.push((id, *a, disk_layout.len() as i32));
-                add_file(&mut disk_layout, *a, id);
-                add_free_space(&mut disk_layout, *b);
-            }
-            [a] => {
-                files.push((id, *a, disk_layout.len() as i32));
-                add_file(&mut disk_layout, *a, id);
-            }
-            _ => unreachable!(),
-        }
-        id += 1;
-    }
-    let original_disk_layout = disk_layout.clone();
+fn calculate_checksum(disk_layout: &Vec<usize>) -> i64 {
+    disk_layout
+        .iter()
+        .enumerate()
+        .filter(|&(_, &value)| value != EMPTY_BLOCK)
+        .map(|(index, &value)| index as i64 * value as i64)
+        .sum()
+}
+
+fn solve_part_one(disk_layout: &mut Vec<usize>) -> i64 {
     let mut l_index = 0;
     let mut r_index = disk_layout.len() - 1;
     while r_index > l_index {
-        if disk_layout[l_index] != i32::MAX {
+        if disk_layout[l_index] != EMPTY_BLOCK {
             l_index += 1;
-        } else if disk_layout[r_index] == i32::MAX {
+        } else if disk_layout[r_index] == EMPTY_BLOCK {
             r_index -= 1;
         } else {
             // Move from right to left.
             disk_layout[l_index] = disk_layout[r_index];
-            disk_layout[r_index] = i32::MAX;
+            disk_layout[r_index] = EMPTY_BLOCK;
             r_index -= 1;
-            l_index += 1;
         }
     }
 
-    let mut checksum: i64 = 0;
-    l_index = 0;
-    loop {
-        let this_value = disk_layout[l_index];
-        if this_value == i32::MAX {
-            break;
-        }
-        checksum += l_index as i64 * this_value as i64;
-        l_index += 1;
-    }
+    calculate_checksum(&disk_layout)
+}
 
-    disk_layout = original_disk_layout;
+fn solve_part_two(disk_layout: &mut Vec<usize>, files: &Vec<(usize, usize, usize)>) -> i64 {
     for (id, len, start_index) in files.into_iter().rev() {
-        // Find first free slot with at least len bytes.
         let mut first_free_slot_index = 0;
         let mut is_gap = false;
-        'outer_loop: while first_free_slot_index < start_index {
+
+        'outer_loop: while first_free_slot_index < *start_index {
             let mut gap_len = 0;
-            for i in 0..len {
-                if disk_layout[(first_free_slot_index + i) as usize] == i32::MAX {
+            for i in 0..*len {
+                if disk_layout[first_free_slot_index + i] == EMPTY_BLOCK {
                     gap_len += 1;
-                    if gap_len == len {
+                    if gap_len == *len {
                         is_gap = true;
                         break 'outer_loop;
                     }
@@ -89,22 +90,21 @@ pub fn solve09(input: &[String]) -> (i128, i128) {
 
         // Move file.
         if is_gap {
-            for i in 0..len {
-                disk_layout[(first_free_slot_index + i) as usize] = id;
-                disk_layout[(start_index + i) as usize] = i32::MAX;
+            for i in 0..*len {
+                disk_layout[(first_free_slot_index + i) as usize] = *id;
+                disk_layout[(start_index + i) as usize] = EMPTY_BLOCK;
             }
         }
     }
 
-    let mut checksum2: i64 = 0;
-    l_index = 0;
-    while l_index < disk_layout.len() {
-        let this_value = disk_layout[l_index];
-        if this_value != i32::MAX {
-            checksum2 += l_index as i64 * this_value as i64;
-        }
-        l_index += 1;
-    }
+    calculate_checksum(&disk_layout)
+}
 
-    (checksum as i128, checksum2 as i128)
+pub fn solve09(input: &[String]) -> (i128, i128) {
+    let (files, mut disk_layout) = parse_input(input);
+
+    (
+        solve_part_one(&mut disk_layout.clone()) as i128,
+        solve_part_two(&mut disk_layout, &files) as i128,
+    )
 }
