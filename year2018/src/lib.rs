5a5ada4a11de9a40ba544027b0f985125dc0e07a
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-12-10 17:43:12 +0000

    2018 day 2

--- year2018/src/lib.rs
+++ year2018/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod solver01;
+pub mod solver02;
 pub mod solver03;
 pub mod solver06;
 pub mod solver10;
@@ -6,6 +7,7 @@ pub mod solver17;
 pub mod solver18;
 
 use crate::solver01::solve01;
+use crate::solver02::solve02;
 use crate::solver03::solve03;
 use crate::solver06::solve06;
 use crate::solver10::solve10;
@@ -20,7 +22,7 @@ pub const fn solve_blank(_input: &[String]) -> (i128, i128) {
 
 pub const SOLVER_FUNCTIONS: [SolverFunction; 25] = [
     solve01,
-    solve_blank,
+    solve02,
     solve03,
     solve_blank,
     solve_blank,
---
+++ year2018/src/solver02.rs
@@ -0,0 +1,56 @@
+use itertools::Itertools;
+use std::collections::HashMap;
+
+fn solve_part_one(input: &[String]) -> i32 {
+    let mut two_count = 0;
+    let mut three_count = 0;
+
+    input.iter().for_each(|line| {
+        let mut chars: HashMap<char, i32> = HashMap::new();
+        line.chars().for_each(|c| {
+            *chars.entry(c).or_insert(0) += 1;
+        });
+
+        if chars.values().contains(&2) {
+            two_count += 1
+        }
+        if chars.values().contains(&3) {
+            three_count += 1
+        }
+    });
+
+    two_count * three_count
+}
+
+fn solve_part_two(input: &[String]) {
+    for i in 0..(input.len() - 1) {
+        for j in i..input.len() {
+            let (diff_count, last_diff_index) = input[i]
+                .chars()
+                .zip(input[j].chars())
+                .enumerate()
+                .filter(|(_, (c1, c2))| c1 != c2)
+                .map(|(index, _)| index)
+                .fold((0, 0), |(mut count, _), index| {
+                    count += 1;
+                    (count, index)
+                });
+
+            if diff_count == 1 {
+                let solution_part_two = input[i]
+                    .chars()
+                    .enumerate()
+                    .filter(|(i, _)| *i != last_diff_index)
+                    .map(|(_, c)| c)
+                    .collect::<String>();
+                println!("{solution_part_two}");
+                return;
+            }
+        }
+    }
+}
+
+pub fn solve02(input: &[String]) -> (i128, i128) {
+    solve_part_two(input);
+    (solve_part_one(input) as i128, 0)
+}
