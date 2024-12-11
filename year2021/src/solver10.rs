5f606fc6a0108752d346edafa7f558fbbc37dbd8
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-12-11 17:37:14 +0000

    2021 day 10 part 2

--- year2021/src/solver10.rs
+++ year2021/src/solver10.rs
@@ -1,10 +1,13 @@
 use aoc::input::to_vec_char;
 
 pub fn solve10(input: &[String]) -> (i128, i128) {
-    let mut score = 0;
+    let mut part_one_score = 0;
     let lines = to_vec_char(input);
+    let mut incomplete_lines: Vec<Vec<char>> = vec![];
+
     lines.iter().for_each(|line| {
         let mut stack: Vec<char> = vec![];
+        let mut line_is_corrupt = false;
         for c in line {
             match c {
                 '(' => stack.push(*c),
@@ -14,34 +17,61 @@ pub fn solve10(input: &[String]) -> (i128, i128) {
                 ')' => {
                     let old_c = stack.pop().unwrap();
                     if old_c != '(' {
-                        score += 3;
+                        part_one_score += 3;
+                        line_is_corrupt = true;
                         break;
                     }
                 }
                 ']' => {
                     let old_c = stack.pop().unwrap();
                     if old_c != '[' {
-                        score += 57;
+                        part_one_score += 57;
+                        line_is_corrupt = true;
                         break;
                     }
                 }
                 '}' => {
                     let old_c = stack.pop().unwrap();
                     if old_c != '{' {
-                        score += 1_197;
+                        part_one_score += 1_197;
+                        line_is_corrupt = true;
                         break;
                     }
                 }
                 '>' => {
                     let old_c = stack.pop().unwrap();
                     if old_c != '<' {
-                        score += 25_137;
+                        part_one_score += 25_137;
+                        line_is_corrupt = true;
                         break;
                     }
                 }
                 _ => unreachable!(),
             }
         }
+        if !line_is_corrupt {
+            incomplete_lines.push(stack);
+        }
     });
-    (score as i128, 0)
+
+    let mut scores: Vec<u64> = vec![];
+
+    incomplete_lines.iter().for_each(|line| {
+        let mut score: u64 = 0;
+        for c in line.iter().rev() {
+            score *= 5;
+            score += match c {
+                '(' => 1,
+                '[' => 2,
+                '{' => 3,
+                '<' => 4,
+                _ => unreachable!(),
+            }
+        }
+        scores.push(score);
+    });
+    scores.sort();
+    let part_two_score = scores[(scores.len() - 1) / 2];
+
+    (part_one_score as i128, part_two_score as i128)
 }
