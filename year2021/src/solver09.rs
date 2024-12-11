8fe8b309777cf2943070a0b68cde83dd1f3840e1
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-12-11 15:32:02 +0000

    2021 day 9 part 2

--- year2021/src/solver09.rs
+++ year2021/src/solver09.rs
@@ -1,22 +1,22 @@
 use aoc::board::Board;
+use itertools::iproduct;
 
 pub fn solve09(input: &[String]) -> (i128, i128) {
     let mut board = Board::from_input(input);
     board.add_border('A');
 
     let mut sum_risk_levels = 0;
-    for r in 1..board.num_rows - 1 {
-        for c in 1..board.num_cols - 1 {
-            if board.cells[r][c] < board.cells[r - 1][c]
-                && board.cells[r][c] < board.cells[r + 1][c]
-                && board.cells[r][c] < board.cells[r][c - 1]
-                && board.cells[r][c] < board.cells[r][c + 1]
-            {
-                sum_risk_levels += 1 + board.cells[r][c].to_digit(10).unwrap();
-            }
+    for (r, c) in iproduct!(1..(board.num_rows - 1), 1..(board.num_cols - 1)) {
+        if board.cells[r][c] < board.cells[r - 1][c]
+            && board.cells[r][c] < board.cells[r + 1][c]
+            && board.cells[r][c] < board.cells[r][c - 1]
+            && board.cells[r][c] < board.cells[r][c + 1]
+        {
+            sum_risk_levels += 1 + board.cells[r][c].to_digit(10).unwrap();
         }
     }
 
+    // Separate the board into either "in a basin" or "between basins".
     board.replace('A', '#');
     board.replace('9', '#');
     board.replace('0', '.');
@@ -28,6 +28,43 @@ pub fn solve09(input: &[String]) -> (i128, i128) {
     board.replace('6', '.');
     board.replace('7', '.');
     board.replace('8', '.');
-    board.print();
-    (sum_risk_levels as i128, 0)
+
+    // Loop over every board cell. Each time an empty cell is found, flood fill the basin it is in.
+    let mut basin_sizes = vec![];
+    let mut next_basin_id = 'a';
+    let mut queue: Vec<(usize, usize)> = vec![];
+
+    for (r, c) in iproduct!(1..(board.num_rows - 1), 1..(board.num_cols - 1)) {
+        if board.cells[r][c] == '.' {
+            // Cycle through letters as basin IDs. We don't actually need a unique ID for each
+            // basin - just a non-empty ID would be fine.
+            if next_basin_id == 'z' {
+                next_basin_id = 'a';
+            }
+            let mut basin_size = 0;
+            queue.push((r, c));
+            while !queue.is_empty() {
+                let (test_r, test_c) = queue.pop().unwrap();
+                [(-1, 0), (0, 1), (1, 0), (0, -1)]
+                    .iter()
+                    .for_each(|(dr, dc)| {
+                        let new_r = (test_r as i32 + dr) as usize;
+                        let new_c = (test_c as i32 + dc) as usize;
+                        if board.cells[new_r][new_c] == '.' {
+                            board.cells[new_r][new_c] = next_basin_id;
+                            basin_size += 1;
+                            queue.push((new_r, new_c));
+                        }
+                    });
+            }
+            basin_sizes.push(basin_size);
+            next_basin_id = ((next_basin_id as u8) + 1) as char;
+        }
+    }
+    basin_sizes.sort();
+    let solution_two = basin_sizes[basin_sizes.len() - 1]
+        * basin_sizes[basin_sizes.len() - 2]
+        * basin_sizes[basin_sizes.len() - 3];
+
+    (sum_risk_levels as i128, solution_two)
 }
