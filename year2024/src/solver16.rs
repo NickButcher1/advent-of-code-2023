cddd01ff501518b209b29f7f1ffb6c609fc19b65
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-12-16 15:19:15 +0000

    Simplify

--- year2024/src/solver16.rs
+++ year2024/src/solver16.rs
@@ -1,5 +1,6 @@
 use aoc::board::Board;
 use itertools::iproduct;
+use std::cmp::Ordering;
 use std::collections::VecDeque;
 use std::collections::{HashMap, HashSet};
 
@@ -11,54 +12,51 @@ const EMPTY: char = '.';
 struct Position {
     r: usize,
     c: usize,
+    // N=0, E=1, S=2, W= 3.
     dir: usize,
 }
 
-pub fn solve16(input: &[String]) -> (i128, i128) {
+fn parse_input(input: &[String]) -> (Board, Position, Position) {
     let mut board = Board::from_input(input);
     let start = board.find(START);
     let end = board.find(END);
-    let end_position = Position {
-        r: end.0,
-        c: end.1,
-        dir: 0,
-    };
 
     board.cells[start.0][start.1] = EMPTY;
     board.cells[end.0][end.1] = EMPTY;
 
+    (
+        board,
+        Position {
+            r: start.0,
+            c: start.1,
+            dir: 1,
+        },
+        Position {
+            r: end.0,
+            c: end.1,
+            dir: 0,
+        },
+    )
+}
+
+pub fn solve16(input: &[String]) -> (i128, i128) {
+    let (board, start_position, end_position) = parse_input(input);
     // Map from position to the lowest score found so far at that position.
     let mut lowest_score: HashMap<Position, usize> = HashMap::new();
-
     // Map from position to all tiles on all lowest-score paths to that position.
     let mut lowest_cells: HashMap<Position, HashSet<(usize, usize)>> = HashMap::new();
-
     // Tips of paths that need to be explored.
-    // N=0, E=1, S=2, W= 3.
     let mut live_points: VecDeque<Position> = VecDeque::new();
-
-    // Initialise the starting state.
-    let start_position = Position {
-        r: start.0,
-        c: start.1,
-        dir: 1,
-    };
     live_points.push_back(start_position.clone());
-    let mut initial_tiles_visited = HashSet::new();
-    initial_tiles_visited.insert((start.0, start.1));
-    lowest_score.insert(start_position.clone(), 0);
-    for (c, r) in iproduct!(0..board.num_cols, 0..board.num_rows) {
-        lowest_cells.insert(Position { r, c, dir: 0 }, HashSet::new());
-        lowest_cells.insert(Position { r, c, dir: 1 }, HashSet::new());
-        lowest_cells.insert(Position { r, c, dir: 2 }, HashSet::new());
-        lowest_cells.insert(Position { r, c, dir: 3 }, HashSet::new());
 
-        lowest_score.insert(Position { r, c, dir: 0 }, usize::MAX);
-        lowest_score.insert(Position { r, c, dir: 1 }, usize::MAX);
-        lowest_score.insert(Position { r, c, dir: 2 }, usize::MAX);
-        lowest_score.insert(Position { r, c, dir: 3 }, usize::MAX);
+    for (c, r, dir) in iproduct!(0..board.num_cols, 0..board.num_rows, 0..=3) {
+        lowest_cells.insert(Position { r, c, dir }, HashSet::new());
+        lowest_score.insert(Position { r, c, dir }, usize::MAX);
     }
-    lowest_cells.insert(start_position.clone(), initial_tiles_visited);
+    lowest_cells.insert(
+        start_position.clone(),
+        HashSet::from([(start_position.r, start_position.c)]),
+    );
     lowest_score.insert(start_position, 0);
 
     while let Some(position) = live_points.pop_front() {
@@ -83,23 +81,31 @@ pub fn solve16(input: &[String]) -> (i128, i128) {
                     if new_score <= *lowest_score.get(&end_position).unwrap() {
                         let old_lowest_score = lowest_score.get(&new_position).unwrap_or(&0);
 
-                        if new_score < *old_lowest_score {
-                            let old_cell_tiles_visited = lowest_cells.get(&position).unwrap();
-                            let mut new_tiles_visited = old_cell_tiles_visited.clone();
-                            new_tiles_visited.insert((new_position.r, new_position.c));
-                            lowest_cells.insert(new_position.clone(), new_tiles_visited);
+                        match new_score.cmp(old_lowest_score) {
+                            Ordering::Less => {
+                                let old_cell_tiles_visited = lowest_cells.get(&position).unwrap();
+                                let mut new_tiles_visited = old_cell_tiles_visited.clone();
+                                new_tiles_visited.insert((new_position.r, new_position.c));
 
-                            lowest_score.insert(new_position.clone(), new_score);
-                            live_points.push_back(new_position);
-                        } else if new_score == *old_lowest_score {
-                            let old_cell_tiles_visited = lowest_cells.get(&position).unwrap();
-                            let new_cell_tiles_visited = lowest_cells.get(&new_position).unwrap();
-                            let mut new_tiles_visited = new_cell_tiles_visited.clone();
-                            new_tiles_visited.extend(old_cell_tiles_visited);
-                            if new_tiles_visited != *new_cell_tiles_visited {
-                                live_points.push_back(new_position.clone());
+                                lowest_cells.insert(new_position.clone(), new_tiles_visited);
+                                lowest_score.insert(new_position.clone(), new_score);
+                                live_points.push_back(new_position);
                             }
-                            lowest_cells.insert(new_position, new_tiles_visited);
+                            Ordering::Equal => {
+                                let old_cell_tiles_visited = lowest_cells.get(&position).unwrap();
+                                let new_cell_tiles_visited =
+                                    lowest_cells.get(&new_position).unwrap();
+
+                                let mut new_tiles_visited = new_cell_tiles_visited.clone();
+                                new_tiles_visited.extend(old_cell_tiles_visited);
+
+                                if new_tiles_visited != *new_cell_tiles_visited {
+                                    live_points.push_back(new_position.clone());
+                                }
+
+                                lowest_cells.insert(new_position, new_tiles_visited);
+                            }
+                            Ordering::Greater => {}
                         }
                     }
                 }
@@ -113,8 +119,8 @@ pub fn solve16(input: &[String]) -> (i128, i128) {
                 i,
                 *lowest_score
                     .get(&Position {
-                        r: end.0,
-                        c: end.1,
+                        r: end_position.r,
+                        c: end_position.c,
                         dir: i,
                     })
                     .unwrap(),
@@ -125,8 +131,8 @@ pub fn solve16(input: &[String]) -> (i128, i128) {
 
     let solution_two = lowest_cells
         .get(&Position {
-            r: end.0,
-            c: end.1,
+            r: end_position.r,
+            c: end_position.c,
             dir: min_dir,
         })
         .unwrap()
