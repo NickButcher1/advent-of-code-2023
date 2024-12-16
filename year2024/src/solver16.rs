25df60fbfc89c21d40a4d25bf5f99d6f55fc5e38
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-12-16 11:25:32 +0000

    2024 day 16 fast

--- year2024/src/solver16.rs
+++ year2024/src/solver16.rs
@@ -1,5 +1,6 @@
 use aoc::board::Board;
 use itertools::iproduct;
+use std::collections::VecDeque;
 use std::collections::{HashMap, HashSet};
 
 const START: char = 'S';
@@ -43,7 +44,7 @@ pub fn solve16(input: &[String]) -> (i128, i128) {
 
     // Tips of paths that need to be explored.
     // N=0, E=1, S=2, W= 3.
-    let mut live_points: Vec<Position> = vec![];
+    let mut live_points: VecDeque<Position> = VecDeque::new();
 
     // Initialise the starting state.
     let start_position = Position {
@@ -51,7 +52,7 @@ pub fn solve16(input: &[String]) -> (i128, i128) {
         c: start.1,
         dir: 1,
     };
-    live_points.push(start_position.clone());
+    live_points.push_back(start_position.clone());
     let mut initial_tiles_visited = HashSet::new();
     initial_tiles_visited.insert((start.0, start.1));
     lowest_score.insert(start_position.clone(), 0);
@@ -69,141 +70,49 @@ pub fn solve16(input: &[String]) -> (i128, i128) {
     lowest_cells.insert(start_position.clone(), initial_tiles_visited);
     lowest_score.insert(start_position, 0);
 
-    let mut loops = 0;
-    while let Some(position) = live_points.pop() {
-        let score = *lowest_score.get(&position).unwrap_or(&0);
-
-        loops += 1;
-        // if loops % 100000 == 0 {
-        println!(
-            "Loop {loops}, live_positons len {}    {:?}",
-            live_points.len(),
-            live_points
-        );
-        let x1 = lowest_score
-            .get(&Position {
-                r: end.0,
-                c: end.1,
-                dir: 0,
-            })
-            .unwrap();
-        let x2 = lowest_score
-            .get(&Position {
-                r: end.0,
-                c: end.1,
-                dir: 1,
-            })
-            .unwrap();
-        let x3 = lowest_score
-            .get(&Position {
-                r: end.0,
-                c: end.1,
-                dir: 2,
-            })
-            .unwrap();
-        let x4 = lowest_score
-            .get(&Position {
-                r: end.0,
-                c: end.1,
-                dir: 3,
-            })
-            .unwrap();
-        let y1 = lowest_cells
-            .get(&Position {
-                r: end.0,
-                c: end.1,
-                dir: 0,
-            })
-            .unwrap()
-            .len();
-        let y2 = lowest_cells
-            .get(&Position {
-                r: end.0,
-                c: end.1,
-                dir: 1,
-            })
-            .unwrap()
-            .len();
-        let y3 = lowest_cells
-            .get(&Position {
-                r: end.0,
-                c: end.1,
-                dir: 2,
-            })
-            .unwrap()
-            .len();
-        let y4 = lowest_cells
-            .get(&Position {
-                r: end.0,
-                c: end.1,
-                dir: 3,
-            })
-            .unwrap()
-            .len();
-        println!("    {x1}  {y1}");
-        println!("    {x2}  {y2}");
-        println!("    {x3}  {y3}");
-        println!("    {x4}  {y4}");
-        // }
-
+    while let Some(position) = live_points.pop_front() {
+        // Try:
+        // - Rotate 90deg left
+        // - Rotate 90deg right
+        // - No rotate which is free.
+        // But don't rotate 180deg because we just came from there.
         for new_dir in 0..=3 {
-            if new_dir != position.dir {
-                let new_position = Position {
+            if ((new_dir + 2) % 4) != position.dir {
+                // First, try all four rotations in place.
+                let rotated_position = Position {
                     r: position.r,
                     c: position.c,
                     dir: new_dir,
                 };
-                let next_position = move_position(&new_position);
-                if board.cells[next_position.r][next_position.c] == EMPTY {
-                    let old_lowest_score = *lowest_score.get(&new_position).unwrap_or(&0);
-                    let new_score = score + 1000;
+                let new_position = move_position(&rotated_position);
+
+                // Now move, but don't move into a wall.
+                if board.cells[new_position.r][new_position.c] == EMPTY {
+                    let new_score = *lowest_score.get(&position).unwrap_or(&0)
+                        + if new_dir != position.dir { 1001 } else { 1 };
 
                     if new_score <= *lowest_score.get(&end_position).unwrap() {
-                        if new_score < old_lowest_score {
+                        let old_lowest_score = lowest_score.get(&new_position).unwrap_or(&0);
+
+                        if new_score < *old_lowest_score {
                             let old_cell_tiles_visited = lowest_cells.get(&position).unwrap();
-                            let new_tiles_visited = old_cell_tiles_visited.clone();
+                            let mut new_tiles_visited = old_cell_tiles_visited.clone();
+                            new_tiles_visited.insert((new_position.r, new_position.c));
                             lowest_cells.insert(new_position.clone(), new_tiles_visited);
 
                             lowest_score.insert(new_position.clone(), new_score);
-                            live_points.push(new_position);
-                        } else if new_score == old_lowest_score {
+                            live_points.push_back(new_position);
+                        } else if new_score == *old_lowest_score {
                             let old_cell_tiles_visited = lowest_cells.get(&position).unwrap();
                             let new_cell_tiles_visited = lowest_cells.get(&new_position).unwrap();
                             let mut new_tiles_visited = new_cell_tiles_visited.clone();
                             new_tiles_visited.extend(old_cell_tiles_visited);
                             if new_tiles_visited != *new_cell_tiles_visited {
-                                live_points.push(new_position.clone());
+                                live_points.push_back(new_position.clone());
                             }
                             lowest_cells.insert(new_position, new_tiles_visited);
                         }
                     }
-                }
-            }
-        }
-
-        let new_position = move_position(&position);
-
-        if board.cells[new_position.r][new_position.c] == EMPTY {
-            let old_lowest_score = lowest_score.get(&new_position).unwrap_or(&0);
-            let new_score = score + 1;
-            if new_score <= *lowest_score.get(&end_position).unwrap() {
-                if new_score < *old_lowest_score {
-                    let old_cell_tiles_visited = lowest_cells.get(&position).unwrap();
-                    let mut new_tiles_visited = old_cell_tiles_visited.clone();
-                    new_tiles_visited.insert((new_position.r, new_position.c));
-                    lowest_cells.insert(new_position.clone(), new_tiles_visited);
-
-                    lowest_score.insert(new_position.clone(), new_score);
-                    live_points.push(new_position);
-                } else if new_score == *old_lowest_score {
-                    let old_cell_tiles_visited = lowest_cells.get(&position).unwrap();
-                    let new_cell_tiles_visited = lowest_cells.get(&new_position).unwrap();
-                    let mut new_tiles_visited = new_cell_tiles_visited.clone();
-                    new_tiles_visited.extend(old_cell_tiles_visited);
-                    if new_tiles_visited != *new_cell_tiles_visited {
-                        live_points.push(new_position.clone());
-                    }
-                    lowest_cells.insert(new_position, new_tiles_visited);
                 }
             }
         }
