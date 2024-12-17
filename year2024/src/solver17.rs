4c056baa83dd893a6177d6b487918ad881e982d9
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-12-17 11:42:25 +0000

    2024 day 17 cleanup

--- year2024/src/solver17.rs
+++ year2024/src/solver17.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 #[derive(Clone, Debug)]
 struct Computer {
     a: u64,
@@ -8,97 +10,111 @@ struct Computer {
 
 #[allow(dead_code)]
 impl Computer {
-    // Inspection of the sample and actual program shows that it:
-    // - Manipulates the registers over multiple steps.
-    // - Outputs a value.
-    // - Jumps to instruction pointer zero if a is non-zero.
+    // This is a vast simplification of the computer architecture defined in the puzzle.
+    // It simply maps from some value of a to some output value, and returns true if the program
+    // should continue.
+    //
+    // Inspection of the sample and actual programs shows that both follow this pattern:
+    // - Manipulate the registers over multiple steps.
+    // - Output a value.
+    // - Jump to instruction pointer zero if a is non-zero, otherwise halt.
+    // Registers b and c are never used as input - they are always derived from some combination of
+    // register a and the program instructions.
     fn execute_program_once(&mut self) -> bool {
         if !self.is_sample {
             let partial = (self.a % 8) ^ 7;
-            let c = self.a / (2u64.pow(partial as u32) as u64); // 75
+            let c = self.a / 2u64.pow(partial as u32); // 75
             let output = ((partial ^ c) ^ 7) % 8; // 40 17
-            self.a = self.a / 8; // 03
-
-            // let output = self.b % 8;
+            self.a /= 8; // 03
             self.output.push(output); // 55
         } else {
-            // TODO Change back for part_one sample.
-            // self.a = self.a / 2; // 01
-            self.a = self.a / 8; // 03
+            self.a /= 8; // 03  For the part two sample, change the 8 to a 2.
             self.output.push(self.a % 8); // 54
-            // 30 no-op
         }
         self.a != 0
     }
 }
 
-fn parse_input(input: &[String]) -> Computer {
-    // TODO: Parse input file.
-    // TODO: Replace is_part_two with something better.
-    let is_sample = false;
-    if is_sample {
-        // Part one sample
-        // Computer { a: 729, b: 0, c: 0, program: vec![0,1,5,4,3,0], is_sample: true, output: vec![]}
-        // Part two sample
-        Computer { a: 2024, program: vec![0,3,5,4,3,0], is_sample: true, output: vec![]}
-    } else {
-        Computer { a: 62769524, program: vec![2,4,1,7,7,5,0,3,4,0,1,7,5,5,3,0], is_sample: false, output: vec![]}
+fn initialize_computer() -> Computer {
+    // TODO: I have hardcoded the input here, to save time in parsing the input file.
+    // Part one sample
+    // Computer { a: 729, b: 0, c: 0, program: vec![0,1,5,4,3,0], is_sample: true, output: vec![]}
+    // Part two sample
+    // Computer { a: 2024, program: vec![0,3,5,4,3,0], is_sample: true, output: vec![]}
+    Computer {
+        a: 62769524,
+        program: vec![2, 4, 1, 7, 7, 5, 0, 3, 4, 0, 1, 7, 5, 5, 3, 0],
+        is_sample: false,
+        output: vec![],
     }
 }
 
-
-fn solve(input: &[String], a: u64) {
-    let mut computer = parse_input(input);
-    computer.a = a; // Goes wrong!
-    computer.output.clear();
-    println!("DO: a = {}", computer.a);
-    while computer.execute_program_once() {
-        println!("    {computer:?}")
-    }
-    println!("SOLUTION ONE: {computer:?}\n");
+fn solve_part_one() -> String {
+    let mut computer = initialize_computer();
+    while computer.execute_program_once() {}
+    computer
+        .output
+        .iter()
+        .map(|digit| digit.to_string())
+        .join(",")
 }
 
-fn find_a(computer: &mut Computer, min_a: u64, max_a: u64, prev_a: u64, target_index: usize) -> Vec<(u64, u64)> {
+fn find_all_a_that_lead_to_prev_a(
+    computer: &mut Computer,
+    min_a: u64,
+    prev_a: u64,
+    target_index: usize,
+) -> Vec<(u64, u64)> {
     let mut candidates = vec![];
-    for a in min_a..=max_a {
+    // I think this upper bound is right, it certainly works.
+    for a in min_a..=(min_a + 8) {
         computer.a = a;
         computer.output.clear();
         let _not_halted = computer.execute_program_once();
-        if computer.output[0] == computer.program[target_index - 1] {
-            // println!("        Index {target_index}    found {a} -> {computer:?}, offset from min_a {}", a - min_a);
-            if prev_a == computer.a {
-                // There could be multiple solutions here.
-                println!("        Index {target_index}    found {a} -> {computer:?}, offset from min_a {}, matches prev_a", a - min_a);
-                candidates.push((a * 8, a));
-            }
+        if computer.output[0] == computer.program[target_index - 1] && prev_a == computer.a {
+            // Lower bound for next value of a must be 8 times this one, because a is divided by 8
+            // on each program execution.
+            candidates.push((a * 8, a));
         }
     }
     candidates
 }
 
-pub fn solve17(input: &[String]) -> (i128, i128) {
-    let mut computer = parse_input(input);
-    let solution_one = format!("{:?}", computer.output);
-    println!("{solution_one}");
-
+// Recurse backwards, to find the a value(s) that can lead to the required output.
+//
+// Given that every program execution divides a by 8, and the program stops when a reaches zero,
+// we know that the value of a which produces the final output value must be in range 1-7.
+fn solve_part_two() -> u64 {
+    let mut computer = initialize_computer();
     let mut target_index = computer.program.len();
     let mut candidates = vec![(1, 0)];
+
     while target_index != 0 {
-        println!("\nSearch for index {target_index}, num candidates = {}, candidates: {candidates:?}", candidates.len());
         let mut new_candidates = vec![];
         for (min_a, prev_a) in &candidates {
-            // let max_a = min_a * 8;
-            let max_a = min_a + 8;
-            println!("    Candidate: Search for index {target_index} in range of a {min_a} - {max_a}, prev_a {prev_a}");
-            let mut more_a = find_a(&mut computer, *min_a, max_a, *prev_a, target_index);
-            new_candidates.append(&mut more_a);
+            new_candidates.append(&mut find_all_a_that_lead_to_prev_a(
+                &mut computer,
+                *min_a,
+                *prev_a,
+                target_index,
+            ));
         }
 
         target_index -= 1;
         candidates = new_candidates;
     }
 
-    // Answer: 258394985014171
+    // There might be multiple values of a that generate the program, but we only want the lowest
+    // one.
+    candidates[0].1
+}
 
-    (0_i128, 0_i128)
+pub fn solve17(_input: &[String]) -> (i128, i128) {
+    let solution_one = solve_part_one();
+    let solution_two = solve_part_two();
+
+    // Part one solution is a string, which doesn't fit into my framework, so print it.
+    println!("Part one: {solution_one}");
+
+    (0_i128, solution_two as i128)
 }
