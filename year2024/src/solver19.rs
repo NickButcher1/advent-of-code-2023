ad704e84402cad3e5784fd042321c0bd552e48f2
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-12-19 16:10:23 +0000

    2024 day 19 simplify

--- year2024/src/solver19.rs
+++ year2024/src/solver19.rs
@@ -35,24 +35,24 @@ fn count_ways<'a>(
 }
 
 pub fn solve19(input: &[String]) -> Solutions {
-    let towels: HashSet<&str> = input[0].split(", ").filter(|s| !s.is_empty()).collect();
+    let towels: HashSet<&str> = input[0].split(", ").collect();
     let max_towel_len = towels.iter().map(|s| s.len()).max().unwrap_or(0);
     let designs: Vec<&String> = input.iter().skip(2).collect();
 
     let mut cache: HashMap<&str, i64> = HashMap::new();
 
-    let (total_num_possible, total_num_ways) = designs
+    let (total_designs_possible, total_num_ways) = designs
         .iter()
         .map(|design| count_ways(&mut cache, &towels, max_towel_len, design))
-        .fold((0, 0), |(possible, ways), this_design_ways| {
+        .fold((0, 0), |(possible_designs, num_ways), this_design_ways| {
             (
-                possible + (this_design_ways > 0) as i32,
-                ways + this_design_ways,
+                possible_designs + (this_design_ways > 0) as i32,
+                num_ways + this_design_ways,
             )
         });
 
     (
-        Solution::I32(total_num_possible),
+        Solution::I32(total_designs_possible),
         Solution::I64(total_num_ways),
     )
 }
