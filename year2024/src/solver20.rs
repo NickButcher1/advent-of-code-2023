63b4565b5d38b3b8aca004677d4b83948b391555
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-12-20 09:20:23 +0000

    2024 day 20 cleanup, still slow

--- year2024/src/solver20.rs
+++ year2024/src/solver20.rs
@@ -1,8 +1,8 @@
-use std::collections::VecDeque;
-use itertools::iproduct;
 use aoc::board::Board;
 use aoc::int_board::IntBoard;
 use aoc::solution::{Solution, Solutions};
+use itertools::iproduct;
+use std::collections::VecDeque;
 
 const EMPTY: char = '.';
 const WALL: char = '#';
@@ -46,7 +46,7 @@ fn find_lowest_steps_for_board(board: &Board, start: (usize, usize), end: (usize
 
             if board.cells[new_position.r][new_position.c] == EMPTY
                 && new_position.steps
-                < lowest_steps_to_end.cells[new_position.r][new_position.c] as usize
+                    < lowest_steps_to_end.cells[new_position.r][new_position.c] as usize
             {
                 lowest_steps_to_end.cells[new_position.r][new_position.c] =
                     new_position.steps as i32;
@@ -71,40 +71,30 @@ pub fn solve20(input: &[String]) -> Solutions {
     board.cells[start.0][start.1] = EMPTY;
     board.cells[end.0][end.1] = EMPTY;
 
-    // println!("START: {start:?}");
-    // println!("END:   {end:?}");
-
     // First the unmodified board.
     let unmodified_cheapest_path = find_lowest_steps_for_board(&board, start, end);
-    println!("CHEAPEST PATH: {unmodified_cheapest_path}");
 
     // Then try every possible cheat.
     let mut solution_one = 0;
-    let mut savings = vec![0; 10_000];
     for (r, c) in iproduct!(1..(board.num_rows - 1), 1..(board.num_cols - 1)) {
         // Only remove a wall if it has an empty cell either side of it.
-        if board.cells[r][c] == WALL && ((board.cells[r - 1][c] == EMPTY && board.cells[r + 1][c] == EMPTY) || (board.cells[r][c - 1] == EMPTY && board.cells[r][c + 1] == EMPTY))    {
+        if board.cells[r][c] == WALL
+            && ((board.cells[r - 1][c] == EMPTY && board.cells[r + 1][c] == EMPTY)
+                || (board.cells[r][c - 1] == EMPTY && board.cells[r][c + 1] == EMPTY))
+        {
             let mut cheat_board = board.clone();
             cheat_board.cells[r][c] = EMPTY;
             let cheapest_path = find_lowest_steps_for_board(&cheat_board, start, end);
             let saving = unmodified_cheapest_path - cheapest_path;
-            savings[saving as usize] += 1;
             if saving >= 100 {
                 solution_one += 1;
             }
         }
     }
 
-    // println!("\nSAVINGS");
-    // for i in 0..savings.len() {
-    //     if savings[i] != 0 {
-    //         println!("    {i:>4}    {}", savings[i]);
-    //     }
-    // }
-
     // PART TWO
-    // Find cheapest path from start to every empty cell.
-    // Find cheapest path from end to every empty cell.
+    // Find the cheapest cost from the start to every empty cell.
+    // Find the cheapest cost from the end to every empty cell.
     let mut cost_from_start = IntBoard::create_empty(board.num_rows, board.num_cols);
     let mut cost_from_end = IntBoard::create_empty(board.num_rows, board.num_cols);
     for (r, c) in iproduct!(1..(board.num_rows - 1), 1..(board.num_cols - 1)) {
@@ -114,49 +104,53 @@ pub fn solve20(input: &[String]) -> Solutions {
         }
     }
 
-    // cost_from_start.print();
-    // cost_from_end.print();
-
-    // Brute force every possible cheat.
-    board.print();
-    let mut savings = vec![0; 10_000];
+    // Visual inspection of the input shows that there is only a single path from start to end and
+    // it visits every cell exactly once.
+    //
+    // Iterate over all possible pairs of cheat start points (must be on an empty cell) and cheat
+    // end points (must also be on an empty cell, and withing a taxicab distance of 20 from the
+    // cheat start point).
+    //
+    // The cheapest cost for this cheat is the sum of:
+    // - start -> cheat start
+    // - taxtcab distance of cheat start -> cheat end
+    // - cheat end -> end.
     let mut solution_two = 0;
-    for (cheat_start_r, cheat_start_c) in iproduct!(1..(board.num_rows - 1), 1..(board.num_cols - 1)) {
-        let unmodified_cheapest_cost = cost_from_start.cells[cheat_start_r][cheat_start_c] + cost_from_end.cells[cheat_start_r][cheat_start_c];
+    for (cheat_start_r, cheat_start_c) in
+        iproduct!(1..(board.num_rows - 1), 1..(board.num_cols - 1))
+    {
+        let unmodified_cheapest_cost = cost_from_start.cells[cheat_start_r][cheat_start_c]
+            + cost_from_end.cells[cheat_start_r][cheat_start_c];
         if unmodified_cheapest_cost != 0 {
-            for cheat_end_r_delta in -20..=20 {
-                for cheat_end_c_delta in -20..=20 {
-                    let taxicab_distance = (cheat_end_r_delta as i32).abs() + (cheat_end_c_delta as i32).abs();
-                    if taxicab_distance >= 2 && taxicab_distance <= 20 {
-                        let mut best_saving = 0;
-                        let cheat_end_r: i32 = cheat_start_r as i32 + cheat_end_r_delta;
-                        let cheat_end_c: i32 = cheat_start_c as i32 + cheat_end_c_delta;
-                        if cheat_end_r > 0 && cheat_end_c > 0 && cheat_end_r < board.num_rows as i32 && cheat_end_c < board.num_rows as i32 {
-                            if board.cells[cheat_end_r as usize][cheat_end_c as usize] == EMPTY {
-                                let modified_cheapest_cost = cost_from_start.cells[cheat_start_r][cheat_start_c] + cost_from_end.cells[cheat_end_r as usize][cheat_end_c as usize] + taxicab_distance;
-                                let saving = unmodified_cheapest_cost - modified_cheapest_cost;
-                                if saving > best_saving {
-                                    best_saving = saving;
-                                }
-                            }
+            for (cheat_end_r_delta, cheat_end_c_delta) in iproduct!(-20..=20, -20..=20) {
+                let taxicab_distance =
+                    (cheat_end_r_delta as i32).abs() + (cheat_end_c_delta as i32).abs();
+                if (2..=20).contains(&taxicab_distance) {
+                    let mut best_saving = 0;
+                    let cheat_end_r: i32 = cheat_start_r as i32 + cheat_end_r_delta;
+                    let cheat_end_c: i32 = cheat_start_c as i32 + cheat_end_c_delta;
+
+                    if cheat_end_r > 0
+                        && cheat_end_c > 0
+                        && cheat_end_r < board.num_rows as i32
+                        && cheat_end_c < board.num_rows as i32
+                        && board.cells[cheat_end_r as usize][cheat_end_c as usize] == EMPTY
+                    {
+                        let modified_cheapest_cost = cost_from_start.cells[cheat_start_r]
+                            [cheat_start_c]
+                            + cost_from_end.cells[cheat_end_r as usize][cheat_end_c as usize]
+                            + taxicab_distance;
+                        let saving = unmodified_cheapest_cost - modified_cheapest_cost;
+                        if saving > best_saving {
+                            best_saving = saving;
                         }
-                        if best_saving > 0 {
-                            savings[best_saving as usize] += 1;
-                        }
-                        if best_saving >= 100 {
-                            println!("    {cheat_start_r},{cheat_start_c} -> {cheat_end_r},{cheat_end_c}  saving {best_saving}");
-                            solution_two += 1;
-                        }
+                    }
+
+                    if best_saving >= 100 {
+                        solution_two += 1;
                     }
                 }
             }
-        }
-    }
-
-    println!("\nPART TWO SAVINGS");
-    for i in 0..savings.len() {
-        if savings[i] != 0 {
-            println!("    {i:>4}    {}", savings[i]);
         }
     }
 
