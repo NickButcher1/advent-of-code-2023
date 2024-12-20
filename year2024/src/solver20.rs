2c9751ccc0d9fdd98b4e294d602bc892d8020955
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-12-20 17:19:18 +0000

    2024 day 20, 2x speedup by not calculating cost from end

--- year2024/src/solver20.rs
+++ year2024/src/solver20.rs
@@ -61,12 +61,7 @@ fn find_lowest_steps_for_board(board: &Board, start: (usize, usize), end: (usize
     lowest_steps_to_end.cells[end.0][end.1]
 }
 
-fn solve(
-    board: &Board,
-    cost_from_start: &IntBoard,
-    cost_from_end: &IntBoard,
-    is_part_two: bool,
-) -> u32 {
+fn solve(board: &Board, cost_from_start: &IntBoard, is_part_two: bool) -> u32 {
     // Visual inspection of the input shows that there is only a single path from start to end and
     // that it visits every cell exactly once.
     //
@@ -89,6 +84,7 @@ fn solve(
             })
             .collect()
     } else {
+        // Move two cells in a straight line in any direction.
         vec![(-2, 0, 2), (2, 0, 2), (0, -2, 2), (0, 2, 2)]
     };
 
@@ -96,9 +92,7 @@ fn solve(
     for (cheat_start_r, cheat_start_c) in
         iproduct!(1..(board.num_rows - 1), 1..(board.num_cols - 1))
     {
-        let unmodified_cheapest_cost = cost_from_start.cells[cheat_start_r][cheat_start_c]
-            + cost_from_end.cells[cheat_start_r][cheat_start_c];
-        if unmodified_cheapest_cost != 0 {
+        if board.cells[cheat_start_r][cheat_start_c] == EMPTY {
             for (cheat_end_r_delta, cheat_end_c_delta, taxicab_distance) in &cheat_deltas {
                 let cheat_end_r: i32 = cheat_start_r as i32 + cheat_end_r_delta;
                 let cheat_end_c: i32 = cheat_start_c as i32 + cheat_end_c_delta;
@@ -109,11 +103,19 @@ fn solve(
                     && cheat_end_c < board.num_rows as i32
                     && board.cells[cheat_end_r as usize][cheat_end_c as usize] == EMPTY
                 {
-                    let modified_cheapest_cost = cost_from_start.cells[cheat_start_r]
-                        [cheat_start_c]
-                        + cost_from_end.cells[cheat_end_r as usize][cheat_end_c as usize]
-                        + taxicab_distance;
-                    let saving = unmodified_cheapest_cost - modified_cheapest_cost;
+                    // Do the following calculation:
+                    // modified cheapest cost = cost from start to cheat start
+                    //                          + cost from end to cheat end
+                    //                          + taxicab distance
+                    // where cost from end to cheat end is the full cost from start to end minus the
+                    // cost from start to cheat end.
+                    //
+                    // The saving is the full cost from start to end minus the modified cheapest
+                    // cost. The two uses of full cost from start to end cancel out, leaving us with
+                    // this calculation  of the saving.
+                    let saving = cost_from_start.cells[cheat_end_r as usize][cheat_end_c as usize]
+                        - cost_from_start.cells[cheat_start_r][cheat_start_c]
+                        - taxicab_distance;
                     // Change to 50 for sample input.
                     if saving >= 100 {
                         solution += 1;
@@ -139,17 +141,14 @@ pub fn solve20(input: &[String]) -> Solutions {
     // Find the cheapest cost from the start to every empty cell.
     // Find the cheapest cost from the end to every empty cell.
     let mut cost_from_start = IntBoard::create_empty(board.num_rows, board.num_cols);
-    let mut cost_from_end = IntBoard::create_empty(board.num_rows, board.num_cols);
     for (r, c) in iproduct!(1..(board.num_rows - 1), 1..(board.num_cols - 1)) {
         if board.cells[r][c] == EMPTY {
             cost_from_start.cells[r][c] = find_lowest_steps_for_board(&board, start, (r, c));
-            cost_from_end.cells[r][c] = find_lowest_steps_for_board(&board, end, (r, c));
         }
     }
 
-    // PART TWO
-    let solution_one = solve(&board, &cost_from_start, &cost_from_end, false);
-    let solution_two = solve(&board, &cost_from_start, &cost_from_end, true);
+    let solution_one = solve(&board, &cost_from_start, false);
+    let solution_two = solve(&board, &cost_from_start, true);
 
     (Solution::U32(solution_one), Solution::U32(solution_two))
 }
