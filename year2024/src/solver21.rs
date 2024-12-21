24738ec047ca4b095a32b14deee3a36c75cc4440
Nick Butcher <NickButcher1@users.noreply.github.com>
2024-12-21 10:09:40 +0000

    part 1 nearly there

--- year2024/src/solver21.rs
+++ year2024/src/solver21.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 use regex::Regex;
-use aoc::board::Board;
 use aoc::solution::{Solution, Solutions};
 
 #[derive(Debug)]
@@ -29,55 +28,56 @@ pub fn solve21(input: &[String]) -> Solutions {
         );
     }
     println!("CODES: {codes:?}");
-    // Index is desired key on cold. Order U R D L.
-    let mut you_to_cold: [[i32; 5]; 4] = [
-        [0, 1, 0, 1, 2], // A -> U -> A
-        [1, 0, 1, 0, 2], // A -> R -> A
-        [1, 1, 1, 1, 4], // A -> D -> A
-        [1, 2, 1, 2, 6], // A -> L -> A
-    ];
-
-    // Index is desired key on cold. Order U R D L.
-    let mut cold_to_radiation: [[i32; 5]; 4] = [
-        [0, 1, 0, 1, 2], // A -> U -> A
-        [1, 0, 1, 0, 2], // A -> R -> A
-        [1, 1, 1, 1, 4], // A -> D -> A
-        [1, 2, 1, 2, 6], // A -> L -> A
-    ];
 
     // Index is from-to on numeric. Order U R D L A.
-    let mut radiation_to_numeric: HashMap<(i32, i32), [i32; 6]> = HashMap::new();
-    radiation_to_numeric.insert((0, 2), [1, 0, 0, 0, 1, 1]);
-    radiation_to_numeric.insert((0, 10), [0, 1, 0, 0, 1, 1]);
-    radiation_to_numeric.insert((1, 6), [1, 2, 0, 0, 1, 3]);
-    radiation_to_numeric.insert((1, 7), [2, 0, 0, 0, 1, 2]);
-    radiation_to_numeric.insert((2, 9), [2, 1, 0, 0, 1, 3]);
-    radiation_to_numeric.insert((3, 7), [2, 0, 0, 2, 1, 4]);
-    radiation_to_numeric.insert((3, 8), [1, 0, 0, 2, 1, 3]);
-    radiation_to_numeric.insert((3, 9), [2, 0, 0, 0, 1, 2]);
-    radiation_to_numeric.insert((4, 5), [0, 1, 0, 0, 1, 1]);
-    radiation_to_numeric.insert((5, 6), [0, 1, 0, 0, 1, 1]);
-    radiation_to_numeric.insert((5, 7), [1, 0, 0, 1, 1, 2]);
-    radiation_to_numeric.insert((6, 3), [0, 0, 1, 0, 1, 1]);
-    radiation_to_numeric.insert((6, 7), [1, 0, 0, 2, 1, 3]);
-    radiation_to_numeric.insert((6, 9), [1, 0, 0, 0, 1, 1]);
-    radiation_to_numeric.insert((6, 10), [0, 0, 2, 0, 1, 2]);
-    radiation_to_numeric.insert((7, 0), [0, 1, 3, 0, 1, 4]);
-    radiation_to_numeric.insert((7, 9), [0, 2, 0, 0, 1, 2]);
-    radiation_to_numeric.insert((8, 0), [0, 0, 3, 0, 1, 3]);
-    radiation_to_numeric.insert((8, 3), [0, 1, 2, 0, 1, 3]);
-    radiation_to_numeric.insert((8, 10), [0, 1, 3, 0, 1, 4]);
-    radiation_to_numeric.insert((9, 8), [0, 0, 0, 1, 1, 1]);
-    radiation_to_numeric.insert((9, 10), [0, 0, 3, 0, 1, 3]);
-    radiation_to_numeric.insert((10, 0), [0, 0, 0, 1, 1, 1]);
-    radiation_to_numeric.insert((10, 1), [1, 0, 0, 1, 1, 2]);
-    radiation_to_numeric.insert((10, 3), [1, 0, 0, 0, 1, 1]);
-    radiation_to_numeric.insert((10, 4), [2, 0, 0, 2, 1, 4]);
-    radiation_to_numeric.insert((10, 5), [2, 0, 0, 1, 1, 3]);
-    radiation_to_numeric.insert((10, 6), [2, 0, 0, 0, 1, 2]);
-    radiation_to_numeric.insert((10, 8), [3, 0, 0, 1, 1, 4]);
-    radiation_to_numeric.insert((10, 9), [3, 0, 0, 0, 1, 3]);
-    // TODO finish this.
+    // For key move on numeric do steps on right on radition.
+    let mut radiation_to_numeric: HashMap<(i32, i32), Vec<char>> = HashMap::new();
+    radiation_to_numeric.insert((0, 2),vec!['^', 'A']);
+    radiation_to_numeric.insert((0, 10),vec!['>', 'A']);
+    radiation_to_numeric.insert((1, 7),vec!['^', '^', 'A']);
+    radiation_to_numeric.insert((2, 9),vec!['^', '^', '>', 'A']);
+    radiation_to_numeric.insert((3, 7),vec!['^', '^', '<', '<', 'A']);
+    radiation_to_numeric.insert((4, 5),vec!['>', 'A']);
+    radiation_to_numeric.insert((5, 6),vec!['>', 'A']);
+    radiation_to_numeric.insert((6, 10),vec!['v', 'v', 'A']);
+    radiation_to_numeric.insert((7, 9),vec!['>', '>', 'A']);
+    radiation_to_numeric.insert((8, 0),vec!['v', 'v', 'v', 'A']);
+    radiation_to_numeric.insert((9, 8),vec!['<', 'A']);
+    radiation_to_numeric.insert((9, 10),vec!['v', 'v', 'v', 'A']);
+    radiation_to_numeric.insert((10, 0),vec!['<', 'A']);
+    radiation_to_numeric.insert((10, 1),vec!['^', '<', '<', 'A']);
+    radiation_to_numeric.insert((10, 3),vec!['^', 'A']);
+    radiation_to_numeric.insert((10, 4),vec!['^', '^', '<', '<', 'A']);
+    radiation_to_numeric.insert((10, 9),vec!['^', '^', '^', 'A']);
+    // TODO add the rest
+
+    // For key move on radiation do steps on right on cold.
+    let mut cold_to_radiation: HashMap<(char, char), Vec<char>> = HashMap::new();
+    cold_to_radiation.insert(('A', '^'),vec!['<', 'A']);
+    cold_to_radiation.insert(('A', '>'),vec!['v', 'A']);
+    cold_to_radiation.insert(('A', 'v'),vec!['v', '<', 'A']);
+    cold_to_radiation.insert(('A', '<'),vec!['v', '<', '<', 'A']);
+    cold_to_radiation.insert(('A', 'A'),vec!['A']);
+    cold_to_radiation.insert(('^', 'A'),vec!['>', 'A']);
+    cold_to_radiation.insert(('^', 'v'),vec!['v', 'A']);
+    cold_to_radiation.insert(('^', '<'),vec!['v', '<', 'A']);
+    cold_to_radiation.insert(('^', '>'),vec!['v', '>', 'A']);
+    cold_to_radiation.insert(('^', '^'),vec!['A']);
+    cold_to_radiation.insert(('>', 'A'),vec!['^', 'A']);
+    cold_to_radiation.insert(('>', 'v'),vec!['<', 'A']);
+    cold_to_radiation.insert(('>', '<'),vec!['<', '<', 'A']);
+    cold_to_radiation.insert(('>', '^'),vec!['<', '^', 'A']);
+    cold_to_radiation.insert(('>', '>'),vec!['A']);
+    cold_to_radiation.insert(('v', 'A'),vec!['^', '>', 'A']);
+    cold_to_radiation.insert(('v', '^'),vec!['^', 'A']);
+    cold_to_radiation.insert(('v', '<'),vec!['<', 'A']);
+    cold_to_radiation.insert(('v', '>'),vec!['>', 'A']);
+    cold_to_radiation.insert(('v', 'v'),vec!['A']);
+    cold_to_radiation.insert(('<', 'A'),vec!['>', '>', '^', 'A']);
+    cold_to_radiation.insert(('<', 'v'),vec!['>', 'A']);
+    cold_to_radiation.insert(('<', '>'),vec!['>', '>', 'A']);
+    cold_to_radiation.insert(('<', '^'),vec!['>', '^', 'A']);
+    cold_to_radiation.insert(('<', '<'),vec!['A']);
 
     let mut total_complexity_solution_one = 0;
     for code in codes {
@@ -91,28 +91,35 @@ pub fn solve21(input: &[String]) -> Solutions {
         let move_3 = radiation_to_numeric.get(&(code.digits[1], code.digits[2])).unwrap();
         // Digit 3 -> A
         let move_4 = radiation_to_numeric.get(&(code.digits[2], code.digits[3])).unwrap();
+        let mut moveit = vec!['A'];
+        moveit.extend(move_1);
+        moveit.extend(move_2);
+        moveit.extend(move_3);
+        moveit.extend(move_4);
 
-        let all_moves = vec![move_1, move_2, move_3, move_4].iter()
-            .fold([0, 0, 0, 0, 0, 0], |acc, &[x, y, z, z2, z3, z4]|
-                [acc[0] + x, acc[1] + y, acc[2] + z, acc[3] + z2, acc[4] + z3, acc[5] + z4]
-            );
-        // all_moves is now the number of  UDLRA required on the numeric keypad.
-        println!("{move_1:?}  {move_2:?}  {move_3:?}  {move_4:?}    {all_moves:?}");
+        println!("MOVEIT: len {}  {moveit:?}", moveit.len() - 1);
+        let mut moveit2: Vec<char> = vec!['A'];
+        for i in 0..(moveit.len() - 1) {
+            let from = moveit[i];
+            let to = moveit[i+1];
+            let new_seq = cold_to_radiation.get(&(from, to)).unwrap();
+            // println!("    {from} -> {to}   new seq {new_seq:?}");
+            moveit2.extend(new_seq);
+        }
+        println!("MOVEIT2: len {}  {moveit2:?}", moveit2.len() - 1);
 
-        let move_cr_u = cold_to_radiation[0].map(|x| x * all_moves[0]);
-        let move_cr_r = cold_to_radiation[1].map(|x| x * all_moves[1]);
-        let move_cr_d = cold_to_radiation[2].map(|x| x * all_moves[2]);
-        let move_cr_l = cold_to_radiation[3].map(|x| x * all_moves[3]);
+        let mut moveit3: Vec<char> = vec![];
+        for i in 0..(moveit2.len() - 1) {
+            let from = moveit2[i];
+            let to = moveit2[i+1];
+            // println!("    {from} -> {to}   new seq {new_seq:?}");
+            let new_seq = cold_to_radiation.get(&(from, to)).unwrap();
+            moveit3.extend(new_seq);
+        }
+        println!("MOVEIT3: len {}  {moveit3:?}", moveit3.len());
 
-        let all_moves_cr = vec![move_cr_u, move_cr_r, move_cr_d, move_cr_l].iter()
-            .fold([0, 0, 0, 0, 0], |acc, &[x, y, z, z2, z3]|
-                [acc[0] + x, acc[1] + y, acc[2] + z, acc[3] + z2, acc[4] + z3]
-            );
-        println!("{move_cr_u:?}  {move_cr_r:?}  {move_cr_d:?}  {move_cr_l:?}    {all_moves_cr:?}");
-
-        // let complexity = length_of_shortest_sequence * code.number;
-        // total_complexity_solution_one += complexity;
+        total_complexity_solution_one += code.number * moveit3.len() as i32;
     }
 
-    (Solution::U32(total_complexity_solution_one), Solution::U32(0))
+    (Solution::I32(total_complexity_solution_one), Solution::I32(0))
 }
