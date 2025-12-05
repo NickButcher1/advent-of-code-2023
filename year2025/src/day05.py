4fa143c294831e8b23b6230089ad0250966db0d3
Nick Butcher <NickButcher1@users.noreply.github.com>
2025-12-05 06:00:15 +0000

    2025 day 5 part 2

--- year2025/src/day05.py
+++ year2025/src/day05.py
@@ -1,27 +1,46 @@
+from itertools import product
+
+
+# This can surely be made more efficient. It stops after finding the first pair of ranges it can combine.
+def combine_ranges(ranges: list[tuple[int, int]]) -> list[tuple[int, int]]:
+    ranges = sorted(ranges, key=lambda x: x[0], reverse=False)
+    for i, j in product(range(len(ranges)), range(len(ranges))):
+        if i < j and ranges[j][0] <= ranges[i][1]:
+            new_range = (ranges[i][0], max(ranges[i][1], ranges[j][1]))
+            ranges = ranges[:j] + ranges[j + 1 :]
+            ranges = ranges[:i] + ranges[i + 1 :]
+            ranges.append(new_range)
+            return ranges
+
+    return ranges
+
+
 def solve(input_file: str) -> tuple[int, int]:
     with open(input_file) as f:
         raw_lines = f.read().splitlines()
         empty_index = raw_lines.index("")
         good_ranges = [tuple(map(int, s.split("-"))) for s in raw_lines[:empty_index]]
         ids = list(map(int, raw_lines[empty_index + 1 :]))
-        good_ranges_asc_1 = sorted(good_ranges, key=lambda x: x[0], reverse=False)
-        good_ranges_desc_2 = sorted(good_ranges, key=lambda x: x[1], reverse=True)
 
-        print(good_ranges_asc_1)
-        print(good_ranges_desc_2)
-        print(ids)
-
+        # Part one.
         fresh_ids = 0
-
         for id in ids:
-            for i in range(len(good_ranges_asc_1)):
-                if good_ranges_asc_1[i][0] <= id <= good_ranges_asc_1[i][1]:
+            for i in range(len(good_ranges)):
+                if good_ranges[i][0] <= id <= good_ranges[i][1]:
                     fresh_ids += 1
                     break
-                if good_ranges_desc_2[i][0] <= id <= good_ranges_desc_2[i][1]:
-                    fresh_ids += 1
-                    break
-    return fresh_ids, 0
+
+        # Part two.
+        last_len = -1
+        while last_len != len(good_ranges):
+            last_len = len(good_ranges)
+            good_ranges = combine_ranges(good_ranges)
+
+        total_fresh_ids = 0
+        for i in range(len(good_ranges)):
+            total_fresh_ids += good_ranges[i][1] - good_ranges[i][0] + 1
+
+    return fresh_ids, total_fresh_ids
 
 
 print(f"Sample: {solve('../input/2025/input05-sample')}")
