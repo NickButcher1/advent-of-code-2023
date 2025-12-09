c1b4af6538c6f9638ca0f2221c7438f8134c3e0f
Nick Butcher <NickButcher1@users.noreply.github.com>
2025-12-09 07:15:39 +0000

    2025 day 9 part 2
    I tried to write my own ray tracing code, eventually gave up and asked an LLM for a library to use. TIL about shapely.

--- year2025/src/day09.py
+++ year2025/src/day09.py
@@ -1,16 +1,29 @@
+from shapely.geometry import Polygon, box
+
+
 def solve(input_file: str) -> tuple[int, int]:
     with open(input_file) as f:
         rows = [tuple(int(item.strip()) for item in line.rstrip().split(",")) for line in f]
-        # print(rows)
 
-        area_max = 0
+        # Close the polygon loop.
+        rows.append(rows[0])
+
+        area_max_part_one = 0
+        area_max_part_two = 0
         for i in range(len(rows)):
             for j in range(i + 1, len(rows)):
-                area = abs(rows[i][0] - rows[j][0] + 1) * abs(rows[i][1] - rows[j][1] + 1)
-                print(f"Comparing point {i} {rows[i]} to point {j} {rows[j]}: area={area}")
-                if area > area_max:
-                    area_max = area
-    return area_max, 0
+                area = (abs(rows[i][0] - rows[j][0]) + 1) * (abs(rows[i][1] - rows[j][1]) + 1)
+                is_rectangle_in_polygon = box(
+                    min(rows[i][0], rows[j][0]),
+                    min(rows[i][1], rows[j][1]),
+                    max(rows[i][0], rows[j][0]),
+                    max(rows[i][1], rows[j][1]),
+                    rows,
+                ).covered_by(Polygon(rows))
+                area_max_part_one = max(area_max_part_one, area)
+                if area > area_max_part_two and is_rectangle_in_polygon:
+                    area_max_part_two = area
+    return int(area_max_part_one), int(area_max_part_two)
 
 
 print(f"Sample: {solve('../input/2025/input09-sample')}")
