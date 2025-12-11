d8ed888b34f5d93a2bd85dec011262e98de0b01a
Nick Butcher <>
2025-12-11 06:15:29 +0000

    2025 day 11 cleanup

--- year2025/src/day11.py
+++ year2025/src/day11.py
@@ -1,15 +1,3 @@
-# aaa: you hhh
-# you: bbb ccc
-# bbb: ddd eee
-# ccc: ddd eee fff
-# ddd: ggg
-# eee: out
-# fff: out
-# ggg: out
-# hhh: ccc fff iii
-# iii: out
-from pprint import pprint
-
 def read_input(input_file: str) -> dict[str, list[str]]:
     with open(input_file) as f:
         rows = [line.rstrip().split(":") for line in f]
@@ -17,8 +5,6 @@ def read_input(input_file: str) -> dict[str, list[str]]:
     next_nodes: dict[str, list[str]] = {}
     for row in rows:
         next_nodes[row[0]] = row[1].strip().split()
-    # print("FORWARD LOOKUP:")
-    # pprint(next_nodes)
 
     # Build the reverse lookup (which nodes can a node be reached from).
     prev_nodes: dict[str, list[str]] = {}
@@ -29,18 +15,19 @@ def read_input(input_file: str) -> dict[str, list[str]]:
             else:
                 prev_nodes[this_node].append(prev_node)
 
-    # print("REVERSE LOOKUP:")
-    # pprint(prev_nodes)
-
     return prev_nodes
 
-def solve(prev_nodes: dict[str, list[str]], source: str) -> tuple[int, int]:
-    # Start at 'out' and count every path to 'you'. At each step, track only the tip of each path and the
+
+def solve(prev_nodes: dict[str, list[str]], source: str) -> int:
+    # Start at "out" and count every path to "you". At each step, track only the tip of each path and the
     # number of ways to reach that tip so far.
+    # Include whether "dac" and "fft" have been satisfied in the tip node name. For part one, just pretend
+    # they have been visited already.
+    current_paths: dict[str, int] = {}
     if source == "svr":
-        current_paths: dict[str, int] = {"outnn": 1}
+        current_paths["outnn"] = 1
     else:
-        current_paths: dict[str, int] = {"outyy": 1}
+        current_paths["outyy"] = 1
 
     satisfied_source = f"{source}yy"
 
@@ -63,17 +50,19 @@ def solve(prev_nodes: dict[str, list[str]], source: str) -> tuple[int, int]:
                         next_tip_node_nn = f"{next_tip_node}{tip_node[3]}y"
                     else:
                         next_tip_node_nn = f"{next_tip_node}{tip_node[3]}{tip_node[4]}"
-                    new_current_paths[next_tip_node_nn] = new_current_paths.get(next_tip_node_nn, 0) + num_ways
+                    new_current_paths[next_tip_node_nn] = (
+                        new_current_paths.get(next_tip_node_nn, 0) + num_ways
+                    )
         current_paths = new_current_paths
-        # print("Current paths:")
-        # pprint(current_paths)
 
-    part_one_answer = current_paths.get(satisfied_source, 0)
-
-    return part_one_answer, 0
+    return current_paths.get(satisfied_source, 0)
 
 
-print(f"Sample-1: {solve(read_input('../input/2025/input11-sample'), "you")}")
-print(f"Sample-2: {solve(read_input('../input/2025/input11-sample-2'), "svr")}")
-print(f"Answer-1:   {solve(read_input('../input/2025/input11'), "you")}")
-print(f"Answer-2:   {solve(read_input('../input/2025/input11'), "svr")}")
+print(
+    f"Sample: ({solve(read_input('../input/2025/input11-sample'), 'you')}, "
+    f"{solve(read_input('../input/2025/input11-sample-2'), 'svr')})"
+)
+print(
+    f"Answer: ({solve(read_input('../input/2025/input11'), 'you')}, "
+    f"{solve(read_input('../input/2025/input11'), 'svr')})"
+)
